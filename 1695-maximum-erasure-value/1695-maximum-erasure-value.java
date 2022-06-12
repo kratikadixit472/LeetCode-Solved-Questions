8619a01ca13793c1cba1b5b8e0083070821e50f3
Kratika Dixit <kratikadixit472@gmail.com>
2022-06-12 08:36:34 +0530

    Time: 8 ms (96.93%), Space: 87.7 MB (71.71%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -3,18 +3,18 @@ class Solution {
         
         int sum = 0, max = 0, i = 0,  j = 0;
         
-        Set<Integer> set = new HashSet<>();
+        boolean[] set = new boolean[10001];
         
         while(i < nums.length && j<nums.length){
             
-            if(!set.contains(nums[j])){
+            if(!set[nums[j]]){
                 sum += nums[j];
                 max = Math.max(max, sum);
-                set.add(nums[j++]);
+                set[nums[j++]] = true;
             }
             else{
                 sum = sum - nums[i];
-                set.remove(nums[i++]);
+                set[nums[i++]] = false;
             }
         }
         return max;
