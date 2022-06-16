13a77e975925dc53a88f569a6c54bf7e353e8874
Kratika Dixit <kratikadixit472@gmail.com>
2022-06-16 12:45:56 +0530

    Time: 2 ms (64.47%), Space: 44.4 MB (15.39%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,41 +1,21 @@
 class Solution {
     
-    List<String> al = new ArrayList<>();
-    
+    List<String> ans = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
         
-        List<String> ans = new ArrayList<>();
+        backtrack("", 0, 0, n);
         
-        dfs(n*2, "", n);
-        
-        for(String s : al){
-            if(isValid(s)) ans.add(s);
-        }
         return ans;
     }
     
-    private void dfs(int n, String str, int N){
-        if(str.length() == 2*N) {
-            al.add(str);
+    private void backtrack(String s, int open, int close, int n){
+        
+        if(s.length() == 2*n) {
+            ans.add(s);
+            return;
         }
-        if(n <= 0) return;
         
-        dfs(n-1, str+"(", N);
-        dfs(n-1, str+")", N);
-    }
-    
-    private boolean isValid(String s){
-        
-        Stack<Character> st = new Stack<>();
-        
-        for(char ch : s.toCharArray()){
-            if(ch == '(') st.push(ch);
-            else{
-                if(st.isEmpty() || st.peek() == ')') return false;
-                st.pop();
-            }
-        }
-        return (st.isEmpty());
-    
+        if(open < n) backtrack(s+"(", open+1, close, n);
+        if(close < open) backtrack(s+")", open, close+1, n);
     }
 }
