ccbe667ac830fdc7d0790ddf5d2141517c84e2e5
Kratika Dixit <kratikadixit472@gmail.com>
2022-06-27 05:50:22 +0530

    Time: 32 ms (62.64%), Space: 54.2 MB (36.84%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -2,21 +2,15 @@ class Solution {
     public String minRemoveToMakeValid(String s) {
         
         Stack<Integer> st = new Stack<>();
-        
         char[] ch = s.toCharArray();
         
-        for(int i=0; i<s.length(); i++){
-            char c = ch[i];
-            if(c == '('){
+        for(int i = 0; i < s.length(); i++){
+            if(ch[i] == '('){
                 st.push(i);
             }
-            else if(c == ')'){
-                if(st.isEmpty()){
-                    ch[i] = '?';
-                }
-                else{
-                    st.pop();
-                }
+            else if(ch[i] == ')'){
+                if(st.isEmpty()) ch[i] = '?';
+                else st.pop();
             }
         }
         
@@ -26,9 +20,10 @@ class Solution {
         
         StringBuilder sb = new StringBuilder();
         
-        for(char c : ch){
-            if(c != '?') sb.append(c);
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] != '?') sb.append(ch[i]);
         }
+        
         return sb.toString();
     }
 }
