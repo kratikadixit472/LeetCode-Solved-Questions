59b5b6398f788588882167c8ba11ea81c6370de2
Kratika Dixit <kratikadixit472@gmail.com>
2022-06-28 19:28:44 +0530

    Time: 9 ms (46.96%), Space: 45.1 MB (45.79%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        
+        if(intervals.length <= 1) return true;
+        
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        
+        int[] interval = intervals[0];
+        
+        for(int i = 1; i < intervals.length; i++){
+            
+            if(interval[1] > intervals[i][0]) return false;
+            
+            interval = intervals[i];
+        }
+        return true;
+    }
+}
