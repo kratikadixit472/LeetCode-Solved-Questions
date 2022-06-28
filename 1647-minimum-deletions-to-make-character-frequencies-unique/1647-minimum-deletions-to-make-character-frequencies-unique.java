bb8c7562839f5d61d8e89c608900f35944ac8f29
Kratika Dixit <kratikadixit472@gmail.com>
2022-06-28 19:12:30 +0530

    Time: 23 ms (38.25%), Space: 54.3 MB (62.64%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,23 +1,32 @@
 class Solution {
     public int minDeletions(String s) {
         
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
         int[] frq = new int[26];
-        Set<Integer> set = new HashSet<>();
         
         for(char ch : s.toCharArray()){
             frq[ch - 'a']++;
         }
-      
-        int cnt = 0;
-        for(int i = 0 ;i < 26; i++){
-             
-            while(frq[i] > 0 && set.contains(frq[i])){
-                frq[i]--;
-                cnt++;
-            }
-            set.add(frq[i]);
-           
+        
+        for(int freq : frq){
+            if(freq > 0) pq.add(freq);
         }
-        return cnt;
+        
+        int ans = 0;
+        
+        while(pq.size() > 1){
+            
+            int topEle = pq.poll();
+            
+            if(topEle == pq.peek()){
+                
+                if(topEle - 1 > 0){
+                    pq.add(topEle - 1);
+                }
+                ans++;
+            }
+        }
+        return ans;
     }
 }
