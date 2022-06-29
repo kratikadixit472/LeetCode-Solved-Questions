2d5c841840f1986ffc6612e885873eb2affcbf8f
Kratika Dixit <kratikadixit472@gmail.com>
2022-06-29 05:41:23 +0530

    Time: 5 ms (35.75%), Space: 46 MB (5.49%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.java
@@ -4,15 +4,21 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         Map<String, List<Integer>> map = new HashMap<>();
         
-        if(map.containsKey(expression)) return map.get(expression);
+        //if(map.containsKey(expression)) return map.get(expression);
         
         for(int i = 0; i < expression.length(); i++){
             
             char ch = expression.charAt(i);
             if(ch == '+' || ch == '-' || ch == '*'){
                 
-                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
-                List<Integer> right = diffWaysToCompute(expression.substring(i+1));
+                List<Integer> left = new ArrayList<>();
+                if(map.containsKey(expression.substring(0, i))) left = map.get(expression.substring(0, i));
+                else left = diffWaysToCompute(expression.substring(0, i));
+                
+                List<Integer> right = new ArrayList<>();
+                
+                if(map.containsKey(expression.substring(i+1))) left = map.get(expression.substring(i+1));
+                else right = diffWaysToCompute(expression.substring(i+1));
                 
                 for(int a : left){
                     for(int b : right){
