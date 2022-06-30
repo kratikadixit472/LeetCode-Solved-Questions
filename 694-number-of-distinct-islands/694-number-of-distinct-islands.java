f925c370b3de1cc5728bbda5e79acba7deb60f09
Kratika Dixit <kratikadixit472@gmail.com>
2022-06-30 19:20:23 +0530

    Time: 40 ms (13.35%), Space: 53.4 MB (50.17%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.java
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.java
@@ -1,39 +1,30 @@
 class Solution {
+    
     public int numDistinctIslands(int[][] grid) {
-        
+
         Set<String> set = new HashSet<>();
-        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0,1}};
-        
-        String[] dir_str = {"u", "l", "d", "r"};
-        
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0 ; j < grid[0].length ; j++){
-                if(grid[i][j] == 1){
-                    
+        for(int i = 0; i < grid.length; i++) {
+             for(int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] != 0) {
                     StringBuilder sb = new StringBuilder();
-                    sb.append("s");
-                    dfs_distinct(grid, i, j, dir, dir_str, sb);
+                    dfs(grid, i, j, sb, "o"); // origin
+                    grid[i][j] = 0;
                     set.add(sb.toString());
                     System.out.print(sb.toString() +" ");
-                    
                 }
             }
         }
         return set.size();
     }
-    
-    private void dfs_distinct(int[][] grid, int i, int j, int[][] dir, String[] dir_str, StringBuilder sb){
-        
+    private void dfs(int[][] grid, int i, int j, StringBuilder sb, String dir) {
+        if(i < 0 || i == grid.length || j < 0 || j == grid[i].length 
+           || grid[i][j] == 0) return;
+        sb.append(dir);
         grid[i][j] = 0;
-        
-        for(int d = 0;  d < 4; d++){
-            int x = i + dir[d][0];
-            int y = j + dir[d][1];
-            
-            if(x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == 1){
-                dfs_distinct(grid, x, y, dir, dir_str, sb.append(dir_str[d]));
-            }
-            if(d == 3) sb.append("e");
-        }
+        dfs(grid, i-1, j, sb, "u");
+        dfs(grid, i+1, j, sb, "d");
+        dfs(grid, i, j-1, sb, "l");
+        dfs(grid, i, j+1, sb, "r");
+        sb.append("b"); // back
     }
 }
