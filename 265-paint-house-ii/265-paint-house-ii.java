a5164afa76e11bcf80894c67ce7fb7120facbfb6
Kratika Dixit <kratikadixit472@gmail.com>
2022-07-06 22:01:22 +0530

    Time: 5 ms (40.67%), Space: 45.6 MB (75.75%) - LeetHub

--- 265-paint-house-ii/265-paint-house-ii.java
+++ 265-paint-house-ii/265-paint-house-ii.java
@@ -1,27 +1,48 @@
 class Solution {
     public int minCostII(int[][] costs) {
         int n = costs.length;
-         
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < costs[0].length; j++){
-                int min = Integer.MAX_VALUE;
-                for(int k = 0; k < costs[0].length; k++){
-                    if(k != j){
-                        if(min > costs[i-1][k]){
-                            min = costs[i-1][k];
-                        }
-                    }
-                }
-                costs[i][j] = min + costs[i][j];
+        
+        int least = Integer.MAX_VALUE, sleast = Integer.MAX_VALUE;
+        
+        for(int j = 0; j < costs[0].length; j++){
+            if(least > costs[0][j]){
+                sleast = least;
+                least = costs[0][j];
+            }
+            else if(sleast > costs[0][j]){
+                sleast = costs[0][j];
             }
         }
         
-        int ans = Integer.MAX_VALUE;
+        for(int i = 1; i < n; i++){
+            int nleast = Integer.MAX_VALUE, nsleast = Integer.MAX_VALUE;
         
-        for(int i = 0; i < costs[0].length; i++){
-            ans = Math.min(costs[n-1][i], ans);
+            for(int j = 0; j < costs[0].length; j++){
+                
+                if(least != costs[i-1][j]){
+                    // System.out.println("Yes");
+                    costs[i][j] = costs[i][j] + least;
+                    // System.out.println(costs[i][j] +" ");
+                }
+                else{
+                    costs[i][j] = sleast + costs[i][j];
+                }
+                // System.out.print(i + " " +j +" "+ least + " "+ sleast +" " + costs[i][j] +" ");
+                if(nleast >= costs[i][j]){
+                    // System.out.print("Yes in least ");
+                    nsleast = nleast;
+                    nleast = costs[i][j];
+                    
+                }
+                else if(nsleast >= costs[i][j]){
+                    nsleast = costs[i][j];
+                }
+                // System.out.println( " -> " + nleast +" "+ nsleast +" ");
+            }
+            least = nleast;
+            sleast = nsleast;
         }
         
-        return ans;
+        return least;
     }
 }
