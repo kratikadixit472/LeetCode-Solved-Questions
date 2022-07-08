5e1d63e1732b34aaf643a055788e59e3395e1ebc
Kratika Dixit <kratikadixit472@gmail.com>
2022-07-08 05:41:46 +0530

    Time: 90 ms (56.53%), Space: 44.6 MB (66.22%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
@@ -1,20 +1,22 @@
 class Solution {
     public int getKth(int lo, int hi, int k) {
         
-        int[][] pow = new int[hi - lo + 1][2];
+        //int[][] pow = new int[hi - lo + 1][2];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1] == b[1]) ? b[0] - a[0] : b[1] - a[1]);
         int idx = 0;
         
-        for(int i = lo; i <= hi && idx < pow.length; i++){
-            pow[idx][0] = i;
-            pow[idx++][1] = getPower(i);
+        for(int i = lo; i <= hi; i++){
+            pq.add(new int[] {i, getPower(i)});
+            if(pq.size() > k) pq.poll();
         }
         
-        Arrays.sort(pow, (a, b) -> (a[0] == b[0]) ? a[0] - b[0] : a[1] - b[1]);
+        //Arrays.sort(pow, (a, b) -> (a[0] == b[0]) ? a[0] - b[0] : a[1] - b[1]);
         
         // for(int[] po : pow){
         //     System.out.print(po[0] +" "+ po[1] +", ");
         // }
-        return pow[k-1][0];
+        int[] ans = pq.peek();
+        return ans[0];
         
     }
     
