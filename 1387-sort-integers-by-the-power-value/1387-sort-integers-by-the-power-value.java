be612339c8d76a26cebb665c58142377a8d0e4de
Kratika Dixit <kratikadixit472@gmail.com>
2022-07-08 05:55:48 +0530

    Time: 60 ms (88.96%), Space: 42.7 MB (83.33%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
@@ -1,33 +1,48 @@
 class Solution {
+    class Pair{
+        int num, pow;
+        public Pair(int num, int pow){
+            this.num = num;
+            this.pow = pow;
+        }
+    }
+    
+    Map<Integer, Integer> map = new HashMap<>();
+    
     public int getKth(int lo, int hi, int k) {
         
         //int[][] pow = new int[hi - lo + 1][2];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1] == b[1]) ? b[0] - a[0] : b[1] - a[1]);
-        int idx = 0;
+        // PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[1] == b[1]) ? b[0] - a[0] : b[1] - a[1]);
+        // int idx = 0;
+        
+        map.put(1, 0);
+        List<Pair> ans = new ArrayList<>();
         
         for(int i = lo; i <= hi; i++){
-            pq.add(new int[] {i, getPower(i)});
-            if(pq.size() > k) pq.poll();
+            getPower(i);
+            ans.add(new Pair (i, map.getOrDefault(i, 0)));
+            
+            //if(pq.size() > k) pq.poll();
         }
         
         //Arrays.sort(pow, (a, b) -> (a[0] == b[0]) ? a[0] - b[0] : a[1] - b[1]);
         
         // for(int[] po : pow){
         //     System.out.print(po[0] +" "+ po[1] +", ");
-        // }
-        int[] ans = pq.peek();
-        return ans[0];
+        // } 
+        
+        ans.sort((a, b) -> (a.num == b.num) ? a.num - b.num : a.pow - b.pow);
+        
+        return ans.get(k-1).num;
         
     }
     
     private int getPower(int n){
-        if(n == 1) return 0;
-        
-        int ans = 0;
-        if(n % 2 == 0) ans += getPower(n / 2) + 1;
-        else{
-            ans += getPower(3 * n + 1) + 1;
-        }
-        return ans;
+        if(map.containsKey(n)) return map.get(n);
+         
+        int ans = (n % 2 == 0) ? getPower(n / 2)  :  getPower(3 * n + 1);
+       
+        map.put(n, ans + 1);
+        return map.get(n);
     }
 }
