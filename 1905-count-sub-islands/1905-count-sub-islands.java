cc6d51765c13fc1f31885a38cb93723b442b1dd0
Kratika Dixit <kratikadixit472@gmail.com>
2022-07-19 09:00:10 +0530

    Time: 43 ms (30.45%), Space: 126.5 MB (59.18%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,45 +1,41 @@
 class Solution {
     public int countSubIslands(int[][] grid1, int[][] grid2) {
         
-      int n = grid2.length;
-        int m = grid2[0].length;
+        int ans = 0;
         
-        int count = 0;
+        int n = grid1.length, m = grid1[0].length;
         
-        //int[][] dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+        int[][] dir = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(grid2[i][j] == 1){
-                    if(dfs(i, j, n, m, grid2, grid1)) {
-                       // System.out.println(i + " "+ j+", ");
-                        count++;
-                    }
+                    
+                    if(DFS(i, j, n, m, grid1, grid2, dir)) ans++;
                 }
             }
         }
         
-        return count;
+        return ans;
     }
     
-    public boolean dfs(int i, int j, int n, int m, int[][] grid2, int[][] grid1)
-    {
-       
+    private boolean DFS(int i, int j, int n, int m, int[][] grid1, int[][] grid2, int[][] dir){
+        
+        
+        if(grid1[i][j] != grid2[i][j]) return false;
         boolean result = true;
-        
-        if(i >= 0 && j >= 0 && i < n && j < m && grid2[i][j] == 1){
+        grid2[i][j] = -1;
+        for(int[] d : dir){
+            int r = i + d[0];
+            int c = j + d[1];
             
-        if(grid2[i][j] != grid1[i][j]) return false;
-        
-            grid2[i][j] = 0;
-            boolean up = dfs(i-1, j, n, m, grid2, grid1);
-            boolean left = dfs(i, j-1, n, m, grid2, grid1);
-            boolean down = dfs(i+1, j, n, m, grid2, grid1);
-            boolean right = dfs(i, j+1, n, m, grid2, grid1);
-            
-            if(!up || !left || !down || !right) return false;
+            if(r >= 0 && c >= 0 && r < n && c < m && grid2[r][c] == 1){
+                if(!DFS(r, c, n, m, grid1, grid2, dir)) {
+                    result = false;
+                }
+            }
+          
         }
-        
         return result;
     }
 }
