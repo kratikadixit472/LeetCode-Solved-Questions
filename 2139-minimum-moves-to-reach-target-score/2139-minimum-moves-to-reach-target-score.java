1ff40a1f45e321c9b0d0b7f16a9478d392f27cfd
Kratika Dixit <kratikadixit472@gmail.com>
2022-07-28 20:32:17 +0530

    Time: 1 ms (43.35%), Space: 40.7 MB (72.89%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        
+        int res = 0;
+        
+        while(target > 1 && maxDoubles > 0){
+            if(target % 2 == 0){
+                target /= 2;
+                maxDoubles--;
+            }
+            else{
+                target--;
+            }
+            res++;
+        }
+        
+        return res + target - 1;
+    }
+}
