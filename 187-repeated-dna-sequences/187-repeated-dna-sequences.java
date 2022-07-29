c0d08f44c61fd80974d842a9321b35452aaae184
Kratika Dixit <kratikadixit472@gmail.com>
2022-07-29 07:42:50 +0530

    Time: 23 ms (87.40%), Space: 50.7 MB (94.02%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.java
@@ -4,7 +4,7 @@ class Solution {
         List<String> ans = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
         
-        for(int i =0 ; i + 10 <= s.length(); i++){
+        for(int i =0 ; i < s.length() - 9; i++){
             String str = s.substring(i, i + 10);
             map.put(str, map.getOrDefault(str, 0) + 1);
         }
