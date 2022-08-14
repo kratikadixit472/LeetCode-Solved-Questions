4b26dfed6095e8a9ce8f5ac8e714dec6bddfdc31
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-14 06:20:41 +0530

    Time: 3 ms (66.74%), Space: 43.3 MB (84.52%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.java
@@ -3,7 +3,44 @@ class Solution {
     int[] par, size;
     public int countComponents(int n, int[][] edges) {
         
-        par = new int[n];
+       // if(connections.length < n-1) return -1;
+        
+        List<Integer>[] graph = new ArrayList[n];
+        
+        for(int i = 0; i < n; i++){
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int[] conn : edges){
+            graph[conn[0]].add(conn[1]);
+            graph[conn[1]].add(conn[0]);
+        }
+        
+        int ans = 0;
+        boolean[] vis = new boolean[n];
+        
+        for(int i = 0; i < n; i++){
+            if(!vis[i]){
+        
+                DFS(i, graph, vis );
+                ans++;
+            }
+        }
+       
+        //ans--;
+        return ans;
+    }
+    
+    private void DFS(int src, List<Integer>[] graph, boolean[] vis ){
+        
+        vis[src] = true;
+        
+        for(int e : graph[src]){
+            if(!vis[e]) DFS(e, graph, vis );
+            //else count++;
+        }
+    }
+        /*par = new int[n];
         
         for(int i = 0; i < n; i++){
             par[i] = i;
@@ -23,5 +60,5 @@ class Solution {
     }
     private int findPar(int u){
         return (par[u] == u) ? u : (par[u] = findPar(par[u]));
-    }
+    }*/
 }
