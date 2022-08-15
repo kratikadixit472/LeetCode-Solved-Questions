f5c7feb84321eae8c9bdc0ca0c1b62b3ea422bb7
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-15 22:32:35 +0530

    Time: 2165 ms (5.04%), Space: 146.2 MB (5.04%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.java
@@ -12,16 +12,12 @@ class Solution {
             parent.put(s, s);
         }
         
-        for(int i = 0; i < stones.length; i++){
-        
-        // for(int[] stone1: stones){
+        for(int i = 0; i < stones.length; i++){ 
             for(int j = i+1; j < stones.length; j++){
                 int[] stone1 = stones[i], stone2 = stones[j];
                 String s1 = stone1[0]+" "+ stone1[1];
-            // for(int[] stone2 : stones){
                 if(i == j) continue;
                 
-               // String s2 = stone1[0] +" "+ stone1[1];
                 if(stone1[0] == stone2[0] || stone1[1] == stone2[1]){
                     String s2 = stone2[0] +" "+ stone2[1];
                     union(s1, s2);
