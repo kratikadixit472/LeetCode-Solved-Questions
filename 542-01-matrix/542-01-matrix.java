f2d7be8127afbaafb3ab918b3d6653cd9904f220
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-18 03:46:05 +0530

    Time: 26 ms (38.58%), Space: 72.7 MB (48.62%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,12 +1,16 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         
-        Queue<int[]> q = new LinkedList<>();
-        int[][] dir = {{-1,0}, {0, -1}, {1,0}, {0,1}};
-        boolean[][] vis = new boolean[mat.length][mat[0].length];
+        int n = mat.length, m = mat[0].length;
         
-        for(int i = 0; i < mat.length; i++){
-            for(int j = 0; j < mat[0].length; j++){
+        LinkedList<int[]> q = new LinkedList<>();
+        
+        int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        boolean[][] vis = new boolean[n][m];
+        
+        for(int i =0 ; i< n; i++){
+            for(int j = 0; j < m; j++){
                 if(mat[i][j] == 0){
                     q.add(new int[]{i, j});
                     vis[i][j] = true;
@@ -17,19 +21,23 @@ class Solution {
         while(!q.isEmpty()){
             int sz = q.size();
             while(sz-- > 0){
-                int[] rvtx = q.poll();
+                int[] curr = q.poll();
                 
-                for(int d = 0; d < 4; d++){
-                    int r = rvtx[0] + dir[d][0];
-                    int c = rvtx[1] + dir[d][1];
+                int i = curr[0], j = curr[1];
+                
+                for(int[] d: dir){
+                    int r = i + d[0];
+                    int c = j + d[1];
                     
-                    if(r < 0 || c < 0 || r >= mat.length || c >= mat[0].length || vis[r][c] == true) continue;
-                    vis[r][c] = true;
-                    mat[r][c] = (mat[rvtx[0]][rvtx[1]]) + 1;
-                    q.add(new int[] {r, c});
+                    if(r >= 0 && c >= 0 && r < n && c < m && !vis[r][c] && mat[r][c] != 0){
+                        mat[r][c] += mat[i][j];
+                        vis[r][c] = true;
+                        q.add(new int[]{r, c});
+                    }
                 }
             }
         }
+        
         return mat;
     }
 }
