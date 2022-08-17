c8e0de3b2eae93653608608a8f4809ef348e5c58
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-18 03:41:30 +0530

    Time: 5 ms (34.97%), Space: 47 MB (75.86%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,49 +1,39 @@
 class Solution {
     
-    int[] par, size;
-    
-    private int findPar(int u){
-        return (par[u] == u) ? u : (par[u] = findPar(par[u]));
-    }
-    
     public int maxAreaOfIsland(int[][] grid) {
         
         int n = grid.length, m = grid[0].length;
-        int[][] dir = {{1, 0}, {0,1}, {-1, 0}, {0, -1}};
         
-        par = new int[n * m];
-        size = new int[n * m];
-        for(int i = 0; i < m * n ; i++){
-            par[i] = i;
-            size[i] = 1;
-        }
-        
-        int max = 0;
+        int[][] dir = {{-1,0},{0,-1},{1,0},{0,1}};
+        int maxIsland = 0;
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 1){
-                    
-                    int p1 = findPar(i * m + j);
-                    
-                    for(int[] d : dir){
-                        int r = i + d[0];
-                        int c = j + d[1];
-                        
-                        
-                        if(r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 1){
-                            int p2 = findPar(r * m + c);
-                            if(p1 != p2){
-                                par[p2] = p1;
-                                size[p1] += size[p2];
-                            }
-                        }
-                    }
-                    max = Math.max(max, size[p1]);
+                    int count = DFS(i, j, n, m, grid, dir);
+                    maxIsland = Math.max(maxIsland, count);
                 }
             }
-            
         }
-        return max;
+        
+        return maxIsland;
+    }
+    
+    private int DFS(int sr, int sc, int er, int ec, int[][] grid, int[][] dir){
+        
+        grid[sr][sc] = 0;
+        
+        int count = 0;
+        
+        for(int[] d : dir){
+            
+            int r = sr + d[0];
+            int c = sc + d[1];
+            
+            if(r >= 0 && c >= 0 && r<er && c<ec && grid[r][c] == 1){
+                count += DFS(r, c, er, ec, grid, dir);
+            }
+        }
+        return count + 1;
     }
 }
