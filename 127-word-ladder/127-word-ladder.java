82c9d63a010c65052baa6958d395aa87ad94bb39
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-18 12:53:54 +0530

    Time: 161 ms (53.81%), Space: 92.1 MB (30.61%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,39 +1,43 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
         Set<String> set = new HashSet<>(wordList);
         if(!set.contains(endWord)) return 0;
         
-        Queue<String> queue = new LinkedList<>();
-        queue.add(beginWord);
+        LinkedList<String> q = new LinkedList<>();
+        q.add(beginWord);
         
-        Set<String> visited = new HashSet<>();
-        queue.add(beginWord);
+        Set<String> vis = new HashSet<>();
+        //vis.add(beginWord);
         
-        int changes = 1;
+        int ans = 1;
         
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i = 0; i < size; i++){
-                String word = queue.poll();
-                if(word.equals(endWord)) return changes;
+        while(!q.isEmpty()){
+            int sz = q.size();
+            while(sz-- > 0){
                 
-                for(int j = 0; j < word.length(); j++){
-                    char arr[] = word.toCharArray();
-                    for(int k = 'a'; k <= 'z'; k++){
-                        char old = arr[j];
-                        arr[j] = (char) k;
+                String word = q.poll();
+                
+                if(word.equals(endWord)) return ans;
+                
+                for(int i = 0; i < word.length(); i++){
+                    char[] arr = word.toCharArray();
+                    for(int c = 'a'; c <= 'z'; c++){
+                        
+                        char old = arr[i];
+                        arr[i] = (char) c;
                         
                         String str = new String(arr);
-                        if(set.contains(str) && !visited.contains(str)){
-                            queue.add(str);
-                            visited.add(str);
+                        if(set.contains(str) && !vis.contains(str)){
+                            q.add(str);
+                            vis.add(str);
                         }
-                        arr[j] = old;
+                        arr[i] = old;
                     }
-                    
                 }
+                
             }
-            ++changes;
+            ans++;
         }
         return 0;
     }
