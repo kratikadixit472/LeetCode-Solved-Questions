7b1856348c8d085e2be43b87690b2c1864e733d4
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-18 21:26:04 +0530

    Time: 13 ms (59.97%), Space: 54.4 MB (44.91%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.java
+++ 934-shortest-bridge/934-shortest-bridge.java
@@ -1,71 +1,68 @@
 class Solution {
     
-    LinkedList<int[]> q = new LinkedList<>();
+    //int count = 0;
+    
     public int shortestBridge(int[][] grid) {
         
         int n = grid.length, m = grid[0].length;
         
-        int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        LinkedList<int[]> q = new LinkedList<>();
+        int[][] dir = {{-1, 0} ,{0, -1}, {1, 0}, {0, 1}};
         
         boolean found = false;
         
         for(int i = 0; i < n; i++){
             if(found) break;
-            for(int j = 0 ; j < m ; j++){
-                
-                if(grid[i][j] == 1){
+            for(int j = 0 ; j < m; j++){
+                if(grid[i][j] == 1) {
                     q.add(new int[]{i, j});
-                    DFS(i, j, n, m, grid, dir);
                     found = true;
+                    DFS(i, j, n, m, grid, q, dir);
                     break;
                 }
             }
         }
-        
-        int step = 0;
+       // System.out.println(count);
+        int shortest = 0;
         
         while(!q.isEmpty()){
             int sz = q.size();
             while(sz-- > 0){
-                int[] curr = q.poll();
-                int i = curr[0], j = curr[1];
-                // System.out.print(i + " "+ j +", ");
-                for(int[] d : dir){
+                int[] top = q.poll();
 
-                    int r = i + d[0];
-                    int c = j + d[1];
+                for(int[] d : dir){
+                    int r = top[0] + d[0];
+                    int c = top[1] + d[1];
 
                     if(r >= 0 && c >= 0 && r < n && c < m){
-                        
-                        if(grid[r][c] == 1) {
-                            //System.out.println("int");
-                            return step;
-                        }
-                        
+                        if(grid[r][c] == 1) return shortest;
+
                         if(grid[r][c] == 0){
+                           // System.out.print(r+" "+c+",  ");
                             grid[r][c] = 2;
                             q.add(new int[]{r, c});
                         }
                     }
                 }
-                
             }
-            // System.out.println("out");
-            step++;
+          //  System.out.println();
+            shortest++;
         }
-        return step;
+        return shortest;
     }
     
-    private void DFS(int i, int j, int n, int m, int[][] grid, int[][] dir){
+    private void DFS(int i, int j, int n, int m, int[][] grid, LinkedList<int[]> q, int[][] dir){
         
         grid[i][j] = 2;
+        
         for(int[] d : dir){
-            
             int r = i + d[0];
             int c = j + d[1];
             
             if(r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 1){
-                DFS(r, c, n, m, grid, dir);
+                //count++;
+              //  System.out.println(r+" "+c);
+                DFS(r, c, n, m, grid, q, dir);
                 q.add(new int[]{r, c});
             }
         }
