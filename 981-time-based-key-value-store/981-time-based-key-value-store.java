123dcc491e7aca3cdad946103b4501c659dd6df4
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-19 05:11:44 +0530

    Time: 345 ms (37.49%), Space: 215.2 MB (73.29%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -0,0 +1,59 @@
+class TimeMap {
+
+    class Pair{
+        String value;
+        int timestamp;
+        public Pair(String value, int timestamp){
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+    
+    Map<String, List<Pair>> map;
+    
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!map.containsKey(key)) map.put(key, new ArrayList<>());
+        map.get(key).add(new Pair(value, timestamp));
+    }
+    
+    public String get(String key, int timestamp) {
+        //int time = 0;
+        //String ans = "";
+        
+        // for(Pair p : map.get(key)){
+        //     if(p.timestamp <= timestamp){
+        //         time = p.timestamp;
+        //         ans = p.value;
+        //     }
+        // }
+        if(map.get(key) == null) return "";
+        String ans = binarySearch(map.get(key), timestamp);
+        return ans;
+    }
+    
+    private String binarySearch(List<Pair> al, int timestamp){
+        int l = 0, r = al.size()-1;
+        
+        while(l < r){
+            int mid = (l + r) / 2;
+            if(al.get(mid).timestamp == timestamp) return al.get(mid).value;
+            if(al.get(mid).timestamp < timestamp){
+                if(al.get(mid + 1).timestamp > timestamp) return al.get(mid).value;
+                l = mid + 1;
+            }
+            else r = mid - 1;
+        }
+        return (al.get(l).timestamp > timestamp) ? "" : al.get(l).value;
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
