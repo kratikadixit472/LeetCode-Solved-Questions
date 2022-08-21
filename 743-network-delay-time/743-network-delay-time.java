816cce31932094aef019c82bd0221880f6452fbc
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-21 14:24:27 +0530

    Time: 25 ms (63.48%), Space: 66.5 MB (34.22%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        
+        List<int[]>[] graph = new ArrayList[n+1];
+        
+        for(int i = 0; i <= n; i++){
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int[] time : times){
+            graph[time[0]].add(new int[]{time[1], time[2]});
+        }
+        
+        return dikshtraAlgo(k, n, graph);
+    }
+    
+    private int dikshtraAlgo(int src, int n, List<int[]>[] graph){
+        
+        int[] dis = new int[n+1];
+        boolean[] vis = new boolean[n+1];
+        
+        Arrays.fill(dis, (int)(1e8));
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        
+        pq.add(new int[]{src, 0});
+        
+        while(!pq.isEmpty()){
+                
+            int[] top = pq.poll();
+            
+            int vtx = top[0], wsf = top[1];
+            
+            if(vis[vtx]) continue;
+            
+            vis[vtx] = true;
+            dis[vtx] = wsf;
+                
+            for(int[] e : graph[vtx]){
+                int v = e[0], w = e[1];
+                if(!vis[v]) pq.add(new int[]{v, w + wsf});
+            }
+        }
+        
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 1 ; i <= n; i++){
+            
+            max = Math.max(max, dis[i]);
+            if(max == (int)(1e8)) return -1;
+        }
+        return max;
+    }
+}
