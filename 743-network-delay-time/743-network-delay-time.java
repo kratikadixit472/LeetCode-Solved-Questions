b0c5009a64c87e265353c55652492811944639d6
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-21 16:32:23 +0530

    Time: 31 ms (49.30%), Space: 65.3 MB (51.72%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -18,12 +18,16 @@ class Solution {
         
         int[] dis = new int[n+1];
         boolean[] vis = new boolean[n+1];
+        int[] par = new int[n+1];
+        Arrays.fill(dis, -1);
         
         Arrays.fill(dis, (int)(1e8));
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         
         pq.add(new int[]{src, 0});
+        par[src] = -1;
+        dis[src] = 0;
         
         while(!pq.isEmpty()){
                 
@@ -38,14 +42,13 @@ class Solution {
                 
             for(int[] e : graph[vtx]){
                 int v = e[0], w = e[1];
-                if(!vis[v]) pq.add(new int[]{v, w + wsf});
+                if(!vis[v] && w + wsf < dis[v]) pq.add(new int[]{v, w + wsf});
             }
         }
         
         int max = Integer.MIN_VALUE;
         
         for(int i = 1 ; i <= n; i++){
-            
             max = Math.max(max, dis[i]);
             if(max == (int)(1e8)) return -1;
         }
