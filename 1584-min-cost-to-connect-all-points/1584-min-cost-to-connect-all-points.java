358ea59939c34a1b29279a6efc4d0b9bbec90df8
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-22 08:29:10 +0530

    Time: 796 ms (30.36%), Space: 124.2 MB (30.94%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -3,49 +3,42 @@ class Solution {
     int[] par;
     
     private int findPar(int u){
-        
-        return (par[u] == u) ? u : (par[u] = findPar(par[u]));
+        return ((par[u] == u) ? u : (par[u] = findPar(par[u])));
     }
     
-    
-    private int calDistance(int[][] points, int i, int j){
-        
-        return (Math.abs(points[i][0] - points[j][0]) + (Math.abs(points[i][1] - points[j][1])));
-        
+    private int getdistance(int[][] p, int i, int j){
+        return Math.abs(p[i][0] - p[j][0]) + Math.abs(p[i][1] - p[j][1]);
     }
     
     public int minCostConnectPoints(int[][] points) {
         
-        int n = points.length;
-        
-        List<int[]> connectAllPoints = new ArrayList<>();
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                connectAllPoints.add(new int[] {calDistance(points, i, j), i, j});
-            }
-        }
-        
-        Collections.sort(connectAllPoints, (a, b) -> a[0] - b[0]);
-        
-        par = new int[n];
-        
-        for(int i = 0; i < n; i++) par[i] = i;
+        List<int[]> list = new ArrayList<>();
         
         int cost = 0;
         
-        for(int[] e : connectAllPoints){
-            int u = e[1], v = e[2], w = e[0];
-            
-            int p1 = findPar(u), p2 = findPar(v);
-            
-            if(p1 != p2){
-                par[p1] = par[p2];
-                
-                cost += w;
+        par = new int[points.length];
+        for(int i = 0; i < points.length; i++){
+            par[i] = i;
+        }
+        
+        for(int i = 0 ; i < points.length; i++){
+            for(int j = i+1; j < points.length ; j++){
+                list.add(new int[]{i, j, getdistance(points, i, j)});
             }
         }
         
+        Collections.sort(list, (a, b) -> a[2] - b[2]);
+        
+        for(int i = 0; i < list.size(); i++){
+            int[] curr = list.get(i);
+            int s = curr[0], d = curr[1], dis = curr[2];
+            int p1 = findPar(s), p2 = findPar(d);
+            
+            if(p1 != p2){
+                par[p2] = par[p1];
+                cost += dis;
+            }
+        }
         return cost;
     }
 }
