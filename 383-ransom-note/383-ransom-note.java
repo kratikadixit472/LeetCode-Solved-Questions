9937daa24f621e809d84b5b667f8e2949daf6dad
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-25 08:25:57 +0530

    Time: 20 ms (46.13%), Space: 52.8 MB (19.50%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(char ch : magazine.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        
+        for(char ch : ransomNote.toCharArray()){
+            if(!map.containsKey(ch)) return false;
+            
+            else if(map.get(ch) >= 1){
+                map.put(ch, map.get(ch) - 1);
+            }
+            else return false;
+        }
+        
+        return true;
+    }
+}
