2f738e4623798a0216180494b66bbb721bd29c16
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-26 06:50:32 +0530

    Time: 36 ms (80.48%), Space: 47.8 MB (59.32%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -1,35 +1,41 @@
 class Solution {
     public int minCut(String s) {
+       
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
         
-        return getAllPartitions(s, 0, s.length()-1, new int[s.length()][s.length()]);
-        
-    }
-    
-    private int getAllPartitions(String s, int l, int r, int[][] dp){
-        
-        if(l >= r || isPalindrome(s, l, r)) return 0;
-        
-        int minCut = r - l;
-        
-        if(dp[l][r] != 0) return dp[l][r];
-        
-        for(int i = l ; i <= r; i++){
-            if(isPalindrome(s, l, i)){
-                
-                minCut = Math.min(minCut, 1 + getAllPartitions(s, i+1, r, dp));
+        for(int jump = 0; jump<n; jump++){
+            for(int i=0, j = jump; j < n; j++, i++){
+                if(jump == 0){
+                    dp[i][j] = true;
+                }
+                else if(jump == 1){
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                }
+                else{
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i+1][j-1]);
+                }
             }
         }
-        dp[l][r] = minCut;
         
-        return dp[l][r];
-    }
-    
-    private boolean isPalindrome(String s, int l, int r){
+        int[] store = new int[n];
         
-        while(l < r){
-            if(s.charAt(l++) != s.charAt(r--)) return false;
+        for(int j = 0; j < dp.length; j++){
+            if(dp[0][j]){
+                store[j] = 0;
+            }
+            else{
+                int min = Integer.MAX_VALUE;
+                for(int i = j; i>=1; i--){
+                    if(dp[i][j]){
+                        if(store[i-1] < min){
+                            min = store[i-1];
+                        }
+                    }
+                }
+                store[j] = min + 1;
+            }
         }
-        return true;
-        
+        return store[n-1];
     }
 }
