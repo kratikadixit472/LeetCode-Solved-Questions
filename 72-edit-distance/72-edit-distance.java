7ca7f1affabe7ec14e8c2dcb5fdaac3e8d5f8dcf
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-26 16:25:27 +0530

    Time: 11 ms (33.15%), Space: 45.6 MB (10.62%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        
+        int n = word1.length();
+        int m = word2.length();
+        
+        int[][] dp = new int[n+1][m+1];
+        
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= m; j++){
+                
+                if(i == 0 || j == 0){
+                    dp[i][j] = (i == 0) ? j : i;
+                    continue;
+                }
+                
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j]));
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+}
