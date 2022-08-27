a64ff2081ffd67d4d13bc82fe4cade1b28469997
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-28 05:03:27 +0530

    Time: 1 ms (93.52%), Space: 41.6 MB (95.92%) - LeetHub

--- 256-paint-house/256-paint-house.java
+++ 256-paint-house/256-paint-house.java
@@ -2,27 +2,32 @@ class Solution {
     public int minCost(int[][] costs) {
         
         int n = costs.length;
+        int[][] dp = new int[n][3];
         
         int ans = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < 3; i++){
+            dp[0][i] = costs[0][i];
+        }
         
         for(int i = 1; i < n; i++){
             for(int j = 0; j < 3; j++){
                 
                 if(j == 0) {
-                    costs[i][j] += Math.min(costs[i-1][j+1], costs[i-1][j+2]); 
+                    dp[i][j] = costs[i][j] + Math.min(dp[i-1][j+1], dp[i-1][j+2]); 
                 }
                 else if(j == 1) {
-                    costs[i][j] += Math.min(costs[i-1][j-1], costs[i-1][j+1]); 
+                    dp[i][j] = costs[i][j] + Math.min(dp[i-1][j-1], dp[i-1][j+1]); 
                 }
                 else{
-                    costs[i][j] += Math.min(costs[i-1][j-1], costs[i-1][j-2]); 
+                    dp[i][j] = costs[i][j] + Math.min(dp[i-1][j-1], dp[i-1][j-2]); 
                 }
             }
             
         }
         
-        for(int cost : costs[n-1]){
-            ans = Math.min(cost, ans);
+        for(int k = 0; k < 3; k++){
+            ans = Math.min(dp[n-1][k], ans);
         }
         
         return ans;
