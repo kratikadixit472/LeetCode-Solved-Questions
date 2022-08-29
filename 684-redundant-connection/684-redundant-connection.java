72b4ec373fdbc66086f4889b53c8a6e4cd442ca0
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-30 05:17:29 +0530

    Time: 1 ms (89.63%), Space: 44.1 MB (43.00%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,6 +1,10 @@
 class Solution {
     
-    static int[] par, size;
+    int[] par;
+    
+    private int findPar(int u){
+        return (par[u] == u) ? u : (par[u] = findPar(par[u]));
+    }
     
     public int[] findRedundantConnection(int[][] edges) {
         
@@ -8,32 +12,18 @@ class Solution {
         
         par = new int[n+1];
         
-        for(int i =0 ;i <= n ; i++){
-            par[i] = i;
-        }
+        for(int i = 0; i <= n; i++) par[i] = i;
         
-        for(int[] edge : edges){
-            
-            int p1 = findPar(edge[0]), p2 = findPar(edge[1]);
-            
-            //System.out.print(edge[0] +" parent -> "+ p1 + ", "+edge[1] +" parent -> "+ p2 +", ");
+        for(int e[] : edges){
+            int p1 = findPar(e[0]);
+            int p2 = findPar(e[1]);
             
             if(p1 != p2){
-                //par[p1] = par[p2];
-                union(p1, p2);
+                par[p1] = par[p2];
             }
-            else return edge;
+            else return e;
         }
-        System.out.println();
-        return edges[0];
+        return new int[]{-1,-1};
     }
     
-    private int findPar(int u){
-        
-        return (par[u] == u) ? u : (par[u] = findPar(par[u]));
-    }
-    
-    private void union(int x, int y) {
-        par[x] = par[y];
-    }
 }
