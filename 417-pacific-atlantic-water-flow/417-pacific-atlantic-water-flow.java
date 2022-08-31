2fc7694f018051140bfb3321427b18f7bf3d3f30
Kratika Dixit <kratikadixit472@gmail.com>
2022-08-31 07:52:23 +0530

    Time: 7 ms (75.44%), Space: 54.7 MB (67.77%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,4 +1,7 @@
 class Solution {
+    
+    int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         
         int n = heights.length, m = heights[0].length;
@@ -7,17 +10,17 @@ class Solution {
         Queue<int[]> pacificQueue = new LinkedList<>();
         Queue<int[]> atlanticQueue = new LinkedList<>();
         
+        boolean[][] pacificMat = new boolean[n][m];
+        boolean[][] atlanticMat = new boolean[n][m];
+        
         for(int i = 0; i < m; i++){
-            pacificQueue.add(new int[]{0, i});
-            atlanticQueue.add(new int[]{n-1, i});
+            dfs(0, i, heights, n, m, pacificMat);
+            dfs(n-1, i, heights, n, m, atlanticMat);
         }
         for(int i = 0; i < n; i++){
-            pacificQueue.add(new int[]{i, 0});
-            atlanticQueue.add(new int[]{i, m-1});
+            dfs(i, 0, heights, n, m, pacificMat);
+            dfs(i, m-1, heights, n, m, atlanticMat);
         }
-        
-        boolean[][] pacificMat = bfs(pacificQueue, heights);
-        boolean[][] atlanticMat = bfs(atlanticQueue, heights);
         
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m; j++){
@@ -29,33 +32,23 @@ class Solution {
         return ans;
     }
     
-    private boolean[][] bfs(Queue<int[]> q, int[][] heights){
-        
-        int n = heights.length, m = heights[0].length;
-        
-        boolean[][] mark = new boolean[n][m];
-        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        
-        while(!q.isEmpty()){
-            
-            int[] top = q.poll();
-            int r = top[0], c = top[1];
-            
-            mark[r][c] = true;
-            
-            for(int[] d : dir){
-                int x = r + d[0];
-                int y = c + d[1];
-                
-                if(x >= 0 && y >= 0 && x < n && y < m && !mark[x][y]){
-                    
-                    if(heights[r][c] <= heights[x][y]){
-                        q.add(new int[]{x, y});
-                    }
-                    
+    private boolean[][] dfs(int r, int c, int[][] heights, int n, int m, boolean[][] mark){
+    
+        mark[r][c] = true;
+
+        for(int[] d : dir){
+            int x = r + d[0];
+            int y = c + d[1];
+
+            if(x >= 0 && y >= 0 && x < n && y < m && !mark[x][y]){
+
+                if(heights[r][c] <= heights[x][y]){
+                    dfs(x, y, heights, n, m, mark);
                 }
+
             }
         }
+        
         return mark;
     }
 }
