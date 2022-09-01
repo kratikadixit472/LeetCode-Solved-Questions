d541fe8dad42a3f8653f93b024d0dae5a229cc91
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-01 10:19:10 +0530

    Time: 4 ms (29.73%), Space: 56 MB (67.42%) - LeetHub

--- 302-smallest-rectangle-enclosing-black-pixels/302-smallest-rectangle-enclosing-black-pixels.java
+++ 302-smallest-rectangle-enclosing-black-pixels/302-smallest-rectangle-enclosing-black-pixels.java
@@ -1,21 +1,33 @@
 class Solution {
+    
+    int top , left, right, bottom, n, m;
+    
     public int minArea(char[][] image, int x, int y) {
         
-        int top = x, bottom = x;
-        int left = y, right = y;
+        top = x; bottom = x;
+        left = y; right = y;
+        n = image.length; m = image[0].length;
         
-        for(int i = 0; i < image.length; i++){
-            for(int j = 0; j < image[0].length; j++){
-                if(image[i][j] == '1') {
-                    // System.out.println(top+" "+ bottom+" "+ left +" "+right);
-                    top = Math.min(top, i);
-                    bottom = Math.max(bottom, i+1);
-                    left = Math.min(left, j);
-                    right = Math.max(right, j+1);
-                }
-            }
-        }
+        int[][] dir ={{-1,0}, {0, -1}, {1, 0}, {0, 1}};
+        
+        DFS(x, y, image, dir);
         
         return (right - left) * (bottom - top);
+    }
+    
+    private void DFS(int x, int y, char[][] image, int[][] dir){
+        
+        if(x >= 0 && y >= 0 && x < n && y < m && image[x][y] == '1'){
+            image[x][y] = '0';
+
+            top = Math.min(top, x);
+            bottom = Math.max(bottom, x+1);
+            left = Math.min(left, y);
+            right = Math.max(right, y+1);
+            DFS(x-1, y, image, dir);
+            DFS(x+1, y, image, dir);    
+            DFS(x, y-1, image, dir);
+            DFS(x, y+1, image, dir);
+        }
     }
 }
