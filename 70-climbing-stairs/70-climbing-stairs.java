426ae194ffbb6b8540bda4e3818a1be2a20e9511
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-02 15:20:57 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (41.82%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -5,10 +5,11 @@ class Solution {
         
         int[] dp = new int[n+1];
         
-        dp[1] = 1; 
-        dp[2] = 2;
-        
-        for(int i = 3; i<=n; i++){
+        for(int i = 0; i<=n; i++){
+            if(i <= 2) {
+                dp[i] = i;
+                continue;
+            }
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
