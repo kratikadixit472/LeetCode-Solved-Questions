ea641e84eae283ed260a9efbb91cc0c166f16071
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-03 19:32:06 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (12.76%) - LeetHub

---
+++ 276-paint-fence/276-paint-fence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numWays(int n, int k) {
+        
+        if(n == 2) return k * k;
+        if(n == 1) return k;
+        
+        int[] dp = new int[n+1];
+        
+        dp[1] = k; dp[2] = k * k;
+        
+        for(int i = 3; i <= n; i++){
+            
+            dp[i] = (k - 1) * (dp[i-1] + dp[i-2]);
+        
+        }
+       
+        return dp[n];
+    }
+}
