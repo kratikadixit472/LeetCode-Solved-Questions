c3854f5c4330d9ca59b2f52aac0c601144409975
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-04 11:31:01 +0530

    Time: 6 ms (89.50%), Space: 67.1 MB (11.24%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,38 +1,20 @@
 class Solution {
-    int[][] dp;
-    public int maximalSquare(char[][] mat) {
+    public int maximalSquare(char[][] matrix) {
         
-        int n = mat.length, m = mat[0].length;
-        int res = 0;
-        dp = new int[n][m];
+        int n = matrix.length, m = matrix[0].length;
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(mat[i][j] == '1'){
-                    dp[i][j] = (findSquare(i, j, mat, n, m));
-                    res = Math.max(res, dp[i][j]);
+        int[][] dp = new int[n+1][m+1];
+        
+        int maxLen = 0;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])+1;
+                    maxLen = Math.max(maxLen, dp[i][j]);
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                System.out.print(dp[i][j] +" ");
-            }System.out.println();
-        }
-        return res*res;
+        return maxLen*maxLen;
     }
-    
-    private int findSquare(int r, int c, char[][] mat, int n, int m){
-        
-        if(r < 0 || c < 0 || r >= n || c  >= m || mat[r][c] == '0'){
-            return 0;
-        }
-        
-        if(dp[r][c] != 0) return dp[r][c];
-        
-        dp[r][c] = 1 + Math.min(findSquare(r+1, c+1, mat, n, m), Math.min(findSquare(r+1, c, mat, n, m), findSquare(r, c+1, mat, n, m)));
-        
-        return dp[r][c];
-        
-    } 
 }
