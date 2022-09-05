6496c6e250a82c1e446591bf59036d25253564e2
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-05 15:22:33 +0530

    Time: 1 ms, Memory: 54.8 MB - LeetHub

---
+++ friend-circles/friend-circles.java
@@ -0,0 +1,30 @@
+class Solution {
+    int[] par;
+    
+    public int findCircleNum(int[][] isConnected) {
+        
+        int n = isConnected.length;
+        int provinces = n;
+        
+        par = new int[n];
+        
+        for(int i = 0; i < n; i++) par[i] = i;
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n; j++){
+                if(i != j && isConnected[i][j] == 1){
+                    int p1 = findPar(i), p2 = findPar(j);
+                    if(p1 != p2){
+                        par[p1] = p2;
+                        provinces--;
+                    }
+                }
+            }
+        }
+        return provinces;
+    }
+    
+    private int findPar(int u){
+        return (par[u] == u) ? u : (par[u] = findPar(par[u]));
+    }
+}
