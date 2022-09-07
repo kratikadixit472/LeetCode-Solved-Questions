217cad8244192a7c13ad042a550caaea2391d251
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-07 09:40:13 +0530

    Time: 6 ms (82.89%), Space: 42.3 MB (82.21%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.java
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.java
@@ -2,22 +2,27 @@ class Solution {
     public int distinctSubseqII(String s) {
         
         int n = s.length();
-        int[] dp = new int[n];
+        int[] dp = new int[n+1];
+        int[] last = new int[26];
         
-        Arrays.fill(dp, 1);
+        
+        Arrays.fill(last, -1);
         int ans = 0, mod = (int) 1e9 + 7;
+        dp[0] = 1;
         
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(s.charAt(i) != s.charAt(j)){
-                    dp[i] += dp[j];
-                    dp[i] = dp[i] % mod;
-                }
+            int ch = s.charAt(i) - 'a';
+            
+            dp[i+1] = (2 * dp[i]) % mod;
+            if(last[ch] >= 0){
+                dp[i+1] -= dp[last[ch]];
             }
-            ans += dp[i];
-            ans = ans % mod;
+            dp[i+1] %= mod;
+            last[ch] = i;
         }
+        dp[n]--;
         
-        return ans;
+        if(dp[n] < n) return dp[n] + mod;
+        return dp[n];
     }
 }
