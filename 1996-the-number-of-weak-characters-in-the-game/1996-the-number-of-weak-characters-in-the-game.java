c6cfc73d166905d42660c2f9f0a8c7c9dd936e0f
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-09 07:55:05 +0530

    Time: 201 ms (32.07%), Space: 131.3 MB (40.83%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -2,15 +2,15 @@ class Solution {
     public int numberOfWeakCharacters(int[][] properties) {
         Arrays.sort(properties, (a, b) -> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
         
-        Stack<int[]> st = new Stack<>();
-        int count = 0;
+        // Stack<int[]> st = new Stack<>();
+        int count = 0, maxVal = 0;
         
-        for(int[] prop : properties){
-            while(!st.isEmpty() && st.peek()[1] < prop[1]){
+        for(int i = properties.length-1; i >= 0; i--){
+            if(maxVal > properties[i][1]){
                 count++;
-                st.pop();
+                // st.pop();
             }
-            st.push(prop);
+            maxVal = Math.max(maxVal, properties[i][1]);
         }
         return count;
     }
