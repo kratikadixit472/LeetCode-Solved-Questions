a277539b9ce7846469dcc14cf88ba7ba3bf70f4c
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-13 21:27:46 +0530

    Time: 80 ms (27.46%), Space: 49.5 MB (49.16%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,41 +2,21 @@ class Solution {
     public int jump(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, 20000);
-        return ifYouCanJump(0, nums, dp);
-    }
-    private int ifYouCanJump(int pos, int[] nums, int[] dp){
-        if(pos >= nums.length-1)  {
-            // dp[pos] = 0;
-            return 0;
-        }
-        
-        int furtherJump = Math.min(pos + nums[pos], nums.length-1);
-        
-        if(dp[pos] != 20000) return dp[pos] ;
-        
-        int res = 20000;
-        
-        for(int nextPos = pos+1; nextPos <= furtherJump; nextPos++){
-            // if(nums[nextPos] == 0) continue;
-            res = Math.min(res, 1 + ifYouCanJump(nextPos, nums, dp));
-        }
-        dp[pos] = res;
-        
-        return res ;
+        return ifYouCanJump(nums, dp);
     }
     
-//         private int ifYouCanJump(int[] nums, boolean[] dp){
+    private int ifYouCanJump(int[] nums, int[] dp){
 
-//             dp[nums.length-1] = true;
+        dp[nums.length-1] = 0;
 
-//             for(int pos = nums.length-2 ; pos >= 0; pos--){
-//                 int furtherJump = Math.min(pos + nums[pos], nums.length-1);
-//                 for(int nextPos = pos+1; nextPos <= furtherJump; nextPos++){
+        for(int pos = nums.length-2 ; pos >= 0; pos--){
+            int furtherJump = Math.min(pos + nums[pos], nums.length-1);
+            for(int nextPos = pos+1; nextPos <= furtherJump; nextPos++){
 
-//                     dp[pos] = Math.min(dp[nextPos], dp[pos]);
+                dp[pos] = Math.min(1 + dp[nextPos], dp[pos]);
 
-//                 }
-//             }
-//             return dp[0];
-//         }
+            }
+        }
+        return dp[0];
+    }
 }
