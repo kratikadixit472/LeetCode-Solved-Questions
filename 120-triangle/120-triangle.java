ea0bc971364cd8de0ec0aa69ae1a3319cba4b475
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-14 10:39:37 +0530

    Time: 5 ms (52.76%), Space: 43.8 MB (71.21%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,22 +1,19 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         
-        Integer[][] dp = new Integer[triangle.size()][triangle.get(triangle.size()-1).size()];
+        int m = triangle.get(triangle.size()-1).size();
+        int n = triangle.size();
+        int[] dp = new int[m];
         
-        return  getMinPath(0,0, triangle.size()-1, triangle, dp);
-    }
-    
-    private int getMinPath(int row, int col, int er, List<List<Integer>> triangle, Integer[][] dp){
-        if(row > er || col >= triangle.get(row).size()) return 0;
-        
-        if(dp[row][col] != null) return dp[row][col];
-        
-        int min = (int)(1e9 + 7);
-        
-        for(int i = col; i <= col+1 && i < triangle.get(row).size(); i++){
-            min = Math.min(min, triangle.get(row).get(i) + getMinPath(row+1, i, triangle.size()-1, triangle, dp));
+        for(int i = 0; i < m; i++){
+            dp[i] = triangle.get(n-1).get(i);
         }
-        dp[row][col] = min;
-        return dp[row][col];
+        
+        for(int layer = n-2; layer >= 0; layer--){
+            for(int i = 0; i <= layer; i++){
+                dp[i] = Math.min(dp[i+1], dp[i]) + triangle.get(layer).get(i);
+            }
+        }
+        return dp[0];
     }
 }
