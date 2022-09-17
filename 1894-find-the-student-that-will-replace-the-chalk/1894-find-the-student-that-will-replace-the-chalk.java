f948bee57b5d797c4f5cd662c053a67a56e9203c
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-17 09:20:25 +0530

    Time: 7 ms (31.28%), Space: 82.3 MB (27.01%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -1,16 +1,26 @@
 class Solution {
-    public int chalkReplacer(int[] chalk, int k) {
+    public int chalkReplacer(int[] chalk, int s) {
         
-        int i = 0, n = chalk.length;
-        while(true){
-            if(i == n){
-                i = 0;
-            }
-            if(chalk[i] > k) return i;
-            else{
-                k -= chalk[i];
-                i++;
-            }
+        int n = chalk.length;
+        long k = s;
+        long[] preSum = new long[n];
+        
+        preSum[0] = chalk[0];
+        for(int i = 1; i < n; i++){
+            preSum[i] = preSum[i-1] + chalk[i];
         }
+        k = k % preSum[n-1];
+        
+        if(k == 0) return 0;
+        
+        int l = 0, r = n-1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            
+            if(preSum[mid] - k == 0) return mid+1;
+            else if( k - preSum[mid] < 0) r = mid-1;
+            else l = mid+1;
+        }
+        return l;
     }
 }
