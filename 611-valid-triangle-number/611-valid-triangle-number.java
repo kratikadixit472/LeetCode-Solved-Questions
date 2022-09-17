b2ec93fe528ac9833af094c5f5ad83a250923f76
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-17 08:21:59 +0530

    Time: 46 ms (49.13%), Space: 43.7 MB (67.23%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.java
+++ 611-valid-triangle-number/611-valid-triangle-number.java
@@ -6,25 +6,10 @@ class Solution {
         for(int i = 0; i < n-2; i++){
             int k = i+2;
             for(int j = i+1; j < n-1 && arr[i] != 0; j++){
-                k = binarySearch(k, arr[i] + arr[j], arr);
-                // System.out.println(i+" "+ j +" "+ k);
+                while(k < n && arr[i] + arr[j] > arr[k]) k++;
                 cnt += k - j - 1;
             }
         }
         return cnt;
-    }
-    private int binarySearch(int l, int sum, int[] arr){
-        int r = arr.length-1;
-        
-        while(l <= r){
-            int mid = (l + r) / 2;
-            if(arr[mid] < sum){
-                l = mid + 1;
-            }
-            else{
-                r = mid-1;
-            }
-        }
-        return l;
     }
 }
