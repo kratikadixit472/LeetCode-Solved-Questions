58805940e2af3d237b9bb2c5147d159e0c07bda4
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-18 21:57:16 +0530

    Time: 4 ms (93.53%), Space: 41 MB (94.05%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.java
+++ 269-alien-dictionary/269-alien-dictionary.java
@@ -1,51 +1,48 @@
 class Solution {
     public String alienOrder(String[] words) {
         
-        Map<Character, List<Character>> graph = new HashMap<>();
+        int n = words.length;
         Map<Character, Integer> indegree = new HashMap<>();
+        Map<Character, List<Character>> graph = new HashMap<>();
         
-        for(int i = 0; i < words.length; i++){
-            for(char ch : words[i].toCharArray()){
-                graph.put(ch, new ArrayList<>());
-                indegree.put(ch, 0);
-            } 
-            
+        for(String word : words){
+            for(char c : word.toCharArray()){
+                graph.putIfAbsent(c, new ArrayList<>());
+                indegree.put(c, 0);
+            }
         }
-         
-        for(int i= 0; i < words.length-1; i++){
-            String word1 = words[i], word2 = words[i+1];
+        
+        for(int i = 0; i < n-1; i++){
+            String w1 = words[i], w2 = words[i+1];
             
-            if(word1.length() > word2.length() && word1.startsWith(word2)) return "";
+            if(w1.length() > w2.length() && w1.startsWith(w2)) return "";
             
-            for(int k = 0; k < Math.min(word1.length(), word2.length()); k++){
-                char par = word1.charAt(k), child = word2.charAt(k);
-                if(par != child){
-                    graph.get(par).add(child);
-                    indegree.put(child, indegree.get(child) + 1);
+            for(int k = 0; k < Math.min(w1.length(), w2.length()) ;k++){
+                char c1 = w1.charAt(k), c2 = w2.charAt(k);
+                if(c1 != c2){
+                    graph.get(c1).add(c2);
+                    indegree.put(c2, indegree.getOrDefault(c2, 0) + 1);
                     break;
                 }
             }
         }
-        
+        StringBuilder sb = new StringBuilder();
         LinkedList<Character> q = new LinkedList<>();
         
-        for(Character key : indegree.keySet()){
+        for(char key : indegree.keySet()){
             if(indegree.get(key) == 0) q.add(key);
         }
         
-        
-        StringBuilder sb = new StringBuilder();
-        
         while(!q.isEmpty()){
-            Character ch = q.poll();
-            sb.append(ch);
+            char top = q.poll();
+            sb.append(top);
             
-            for(Character child : graph.get(ch)){
-                indegree.put(child, indegree.get(child)-1);
-                if(indegree.get(child) == 0) q.add(child);
+            for(char c : graph.get(top)){
+                indegree.put(c, indegree.getOrDefault(c, 0) - 1);
+                if(indegree.get(c) == 0) q.add(c);
             }
         }
-        
-        return (sb.length() < indegree.size()) ? "" : sb.toString();
+        if(sb.length() < indegree.size()) return "";
+        return sb.toString();
     }
 }
