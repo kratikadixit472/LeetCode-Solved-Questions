e4df0d58516806c09a4c1f5d1d4fc4dd11889ab2
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-19 01:11:28 +0530

    Time: 24 ms (69.40%), Space: 43.6 MB (56.27%) - LeetHub

--- 839-similar-string-groups/839-similar-string-groups.java
+++ 839-similar-string-groups/839-similar-string-groups.java
@@ -1,50 +1,58 @@
 class Solution {
     
-    int[] par;
-    
-    public int numSimilarGroups(String[] strs) {
-        
-        int n = strs.length;
-        par = new int[n];
-        
-        for(int i = 0 ; i < strs.length; i++){
-            par[i] = i;
-        }
-        
-        int ans = n;
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                
-                if(isSimilar(strs[i], strs[j])){
-                    
-                    int p1 = findPar(i);
-                    int p2 = findPar(j);
-                    
-                    if(p1 != p2){
-                        par[p2] = p1;
-                        ans--;
-                    }
-                }
-                
-            }
-        }
-        return ans;
-    }
+    int[] par, w;
     
     private int findPar(int u){
         return (par[u] == u) ? u : (par[u] = findPar(par[u]));
     }
     
-    private boolean isSimilar(String s1, String s2){
+    private boolean isUnion(int i, int j){
+        int p1 = findPar(i), p2 = findPar(j);
+        if(p1 == p2) return false;
         
-        int cnt = 0;
-        
-        for(int i =0 ; i < s1.length(); i++){
-            if(s1.charAt(i) != s2.charAt(i)) cnt++;
-            
-            if(cnt > 2) return false;
+        if(w[p1] < w[p2]) {
+            w[p2] += w[p1];
+            par[p1] = p2;
+        }
+        else {
+            w[p1] += w[p2];
+            par[p2] = p1;
         }
         return true;
     }
+    
+    private boolean isSimilar(String a, String b){
+        int cnt = 0;
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)) cnt++;
+            if(cnt > 2) return false;
+            
+        }
+        return true;
+    }
+    
+    public int numSimilarGroups(String[] strs) {
+        
+        int n = strs.length;
+        
+        par = new int[n];
+        w = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            par[i] = i;
+            w[i] = 1;
+        }
+        
+        int size = n;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j <n; j++){
+                if(isSimilar(strs[i], strs[j]) && isUnion(i, j)){
+                    size--;
+                }
+            }
+        }
+        return size;
+    }
+    
 }
