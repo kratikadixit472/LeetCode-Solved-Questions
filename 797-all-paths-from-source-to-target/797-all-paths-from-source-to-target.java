87d9b6cd76eef4446cc849044a57fdcec6e0c0ee
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-18 09:24:31 +0530

    Time: 4 ms (72.67%), Space: 55.7 MB (15.07%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,26 +1,30 @@
 class Solution {
-    
-    List<List<Integer>> ans = new ArrayList<>();
+    List<List<Integer>> ans;
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         
+        int n = graph.length;
+        // List<Integer>[] graph = new LinkedList[n];
+        
+        ans = new ArrayList<>();
+        
+        // LinkedList<Integer> q = new LinkedList<>();
         List<Integer> al = new ArrayList<>();
         al.add(0);
-        allPaths(0, graph.length-1, graph, al);
+        DFS(0, n-1, graph, al);
         
         return ans;
     }
-    
-    private void allPaths(int src, int dest, int[][] graph, List<Integer> al){
+    private void DFS(int src, int dest, int[][] graph, List<Integer> al){
         
-        if(src == dest){
+        if(src == dest) {
             ans.add(new ArrayList<>(al));
             return;
         }
         
-        for(int vrt : graph[src]){
-            al.add(vrt);
-            allPaths(vrt, dest, graph, al);
+        for(int ngbr : graph[src]){
+            al.add(ngbr);
+            DFS(ngbr, dest, graph, al);
             al.remove(al.size()-1);
-        }
+        }   
     }
 }
