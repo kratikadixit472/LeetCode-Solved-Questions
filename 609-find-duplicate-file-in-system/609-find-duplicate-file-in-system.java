5c8653e35be0a59b75fb89b53779d54bdeefc765
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-20 03:06:05 +0530

    Time: 27 ms (96.34%), Space: 50.8 MB (98.49%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.java
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.java
@@ -22,6 +22,3 @@ class Solution {
         return ans;
     }
 }
-
-/*Input: paths = ["root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"]
-Output: [["root/a/2.txt","root/c/d/4.txt","root/4.txt"],["root/a/1.txt","root/c/3.txt"]]*/
