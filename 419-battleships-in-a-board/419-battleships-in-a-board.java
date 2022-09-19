892e47329df4264c3540810853c163de949fffc6
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-19 20:26:26 +0530

    Time: 4 ms (10.56%), Space: 43.9 MB (73.08%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.java
+++ 419-battleships-in-a-board/419-battleships-in-a-board.java
@@ -1,39 +1,22 @@
 class Solution {
-    
-    int row, col ;
-    int n, m;
-    
+   
     public int countBattleships(char[][] board) {
         
-        n = board.length; m = board[0].length;
+        int n = board.length, m = board[0].length;
         int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         int ans = 0;
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(board[i][j] == 'X'){
-                    DFS(i, j, board, dir);
-                    if(row == i || col == j){
-                        ans++;
-                    }
+                    if(i > 0 && board[i-1][j] == 'X') continue;
+                    if(j > 0 && board[i][j-1] == 'X') continue;
+                    ans++;
+                    
                 }
             }
         }
         return ans;
     }
     
-    private void DFS(int i, int j, char[][] board, int[][] dir){
-        
-        row = i; col = j;
-        board[i][j] = '.';
-        
-        for(int[] d : dir){
-            int r = i + d[0];
-            int c = j + d[1];
-            
-            if(r >= 0 && c >= 0 && r < n && c < m && board[r][c] == 'X'){
-                DFS(r, c, board, dir);
-            }
-        }
-    }
 }
