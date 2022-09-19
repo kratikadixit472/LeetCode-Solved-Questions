4ba38e99d96db663656b05567b818059418d656f
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-20 03:30:01 +0530

    Time: 22 ms (73.90%), Space: 47.4 MB (65.79%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,43 +1,33 @@
 class Solution {
-    public int shortestPath(int[][] grid, int k) {
-        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    public int shortestPath(int[][] grid, int K) {
         
         int n = grid.length, m = grid[0].length;
-        
-        Queue<int[]> q = new LinkedList<>();
-        
-        q.add(new int[]{0, 0, 0, 0});
+        LinkedList<int[]> q = new LinkedList<>();
+        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         int[][] vis = new int[n][m];
         
-        for(int v[] : vis){
-            Arrays.fill(v, Integer.MAX_VALUE);
-        }
+        for(int[] v : vis) Arrays.fill(v, Integer.MAX_VALUE);
         
         vis[0][0] = 0;
+        q.add(new int[]{0, 0, 0, 0});
         
         while(!q.isEmpty()){
             
             int[] top = q.poll();
-            int i = top[0], j = top[1];
-            int currK = top[2], dis = top[3];
+            int i = top[0], j = top[1], k = top[2], dis = top[3];
             
-            // System.out.println(i+" "+ j+" "+ currK+" "+ dis);
-            
+            // System.out.println(i+" "+ j +" "+ k +" "+ dis);
             if(i == n-1 && j == m-1) return dis;
-            
-            for(int[] d: dir){
+            for(int[] d : dir){
                 int r = i + d[0];
                 int c = j + d[1];
                 
                 if(r >= 0 && c >= 0 && r < n && c < m){
-                    
-                    int newK = currK + grid[r][c];
-                    
-                    if(currK <= k && vis[r][c] > newK){
-                        vis[r][c] = newK;
-                        q.add(new int[]{r, c, newK, dis+1});
+                    int newk = k + grid[r][c];
+                    if(newk <= K && vis[r][c] > newk){
+                        vis[r][c] = newk;
+                        q.add(new int[]{r, c, newk, dis+1});
                     }
-                    
                 }
             }
         }
