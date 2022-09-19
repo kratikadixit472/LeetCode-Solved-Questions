ede3c1e1f7227d535343a792c8147416b8468d59
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-19 23:02:59 +0530

    Time: 148 ms (30.96%), Space: 69.3 MB (72.39%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -1,31 +1,44 @@
 class Solution {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
         
-        List<Integer>[] graph = new ArrayList[numCourses];
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        Map<Integer, Set<Integer>> preq = new HashMap<>();
         
         for(int i = 0; i < numCourses; i++){
-            graph[i] = new ArrayList<>();
+            graph.put(i, new HashSet<>());
+            preq.put(i, new HashSet<>());
         }
+        
+        int[] indegree = new int[numCourses];
         for(int[] p : prerequisites){
-            graph[p[0]].add(p[1]);
+            graph.get(p[0]).add(p[1]);
+            indegree[p[1]]++;
         }
+        
+        LinkedList<Integer> q = new LinkedList<>();
+        
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) q.add(i);
+        }
+        
+        while(!q.isEmpty()){
+            int top = q.poll();
+            Set<Integer> set = graph.get(top);
+            
+            for(int next : set){
+                preq.get(next).add(top);
+                preq.get(next).addAll(preq.get(top));
+                
+                if(--indegree[next] == 0) q.add(next);
+            }
+        }
+        
         List<Boolean> ans = new ArrayList<>();
         
-        for(int[] q : queries){
-            if(DFS(q[0], q[1], new boolean[numCourses], graph)) ans.add(true);
+        for(int[] que : queries){
+            if(preq.get(que[1]).contains(que[0])) ans.add(true);
             else ans.add(false);
         }
         return ans;
-    }
-    private boolean DFS(int src, int dest, boolean[] vis, List<Integer>[] graph){
-        vis[src] = true;
-        if(src == dest) return true;
-        
-        for(int e : graph[src]){
-            if(!vis[e]) {
-                if(DFS(e, dest, vis, graph)) return true;
-            }
-        }
-        return false;
     }
 }
