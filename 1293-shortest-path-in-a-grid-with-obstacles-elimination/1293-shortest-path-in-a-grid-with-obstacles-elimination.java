44bd6a59fdeb6150522e0a7647eea993139ed2f0
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-20 03:33:24 +0530

    Time: 7 ms (94.26%), Space: 42 MB (97.63%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -9,27 +9,31 @@ class Solution {
         for(int[] v : vis) Arrays.fill(v, Integer.MAX_VALUE);
         
         vis[0][0] = 0;
-        q.add(new int[]{0, 0, 0, 0});
+        q.add(new int[]{0, 0, 0});
+        int distance = 0;
         
         while(!q.isEmpty()){
-            
-            int[] top = q.poll();
-            int i = top[0], j = top[1], k = top[2], dis = top[3];
-            
-            // System.out.println(i+" "+ j +" "+ k +" "+ dis);
-            if(i == n-1 && j == m-1) return dis;
-            for(int[] d : dir){
-                int r = i + d[0];
-                int c = j + d[1];
-                
-                if(r >= 0 && c >= 0 && r < n && c < m){
-                    int newk = k + grid[r][c];
-                    if(newk <= K && vis[r][c] > newk){
-                        vis[r][c] = newk;
-                        q.add(new int[]{r, c, newk, dis+1});
+            int sz = q.size();
+            while(sz-- > 0){
+                int[] top = q.poll();
+                int i = top[0], j = top[1], k = top[2];
+
+                // System.out.println(i+" "+ j +" "+ k +" "+ dis);
+                if(i == n-1 && j == m-1) return distance;
+                for(int[] d : dir){
+                    int r = i + d[0];
+                    int c = j + d[1];
+
+                    if(r >= 0 && c >= 0 && r < n && c < m){
+                        int newk = k + grid[r][c];
+                        if(newk <= K && vis[r][c] > newk){
+                            vis[r][c] = newk;
+                            q.add(new int[]{r, c, newk});
+                        }
                     }
                 }
             }
+            distance++;
         }
         return -1;
     }
