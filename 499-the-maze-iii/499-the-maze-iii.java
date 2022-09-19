a096e35963012f64a2c9daa9f00c2d08966649d8
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-19 08:23:42 +0530

    Time: 7 ms (91.57%), Space: 41.8 MB (95.02%) - LeetHub

--- 499-the-maze-iii/499-the-maze-iii.java
+++ 499-the-maze-iii/499-the-maze-iii.java
@@ -1,89 +1,62 @@
 class Solution {
-    class Pair{
-        
-        int r, c;
-        
-        public Pair(int r, int c){
-            this.r = r;
-            this.c = c;
-        }
-    }
     public String findShortestWay(int[][] maze, int[] ball, int[] hole) {
+        
         int n = maze.length, m = maze[0].length;
         
-        Queue<int[]> q = new LinkedList<>();
-        int[][] dir = {{-1,0}, {0,-1}, {0,1}, {1,0}};
-        String[] dirs = {"u", "l", "r", "d"};
+        int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        String[] sdir = {"u", "d", "l", "r"};
         
+        String[][] dpPath = new String[n][m];
         int[][] dis = new int[n][m];
-        String[][] pathdp = new String[n][m];
+        for(int[] d : dis) Arrays.fill(d, Integer.MAX_VALUE);
+        for(String[] s : dpPath) Arrays.fill(s, "z");
         
-        for (String[] d: pathdp) {
-            Arrays.fill(d, "z");
-        }
-        
-        int min = Integer.MAX_VALUE;
-        
-        for(int i = 0; i < n; i++){
-            Arrays.fill(dis[i], Integer.MAX_VALUE);
-        }
-        
-        q.add(new int[] {ball[0], ball[1]});
+        LinkedList<int[]> q = new LinkedList<>();
+        q.add(new int[]{ball[0], ball[1]});
         dis[ball[0]][ball[1]] = 0;
-        pathdp[ball[0]][ball[1]] = "";
+        dpPath[ball[0]][ball[1]] = "";
         
         while(!q.isEmpty()){
             
-            int[] curr = q.poll();
+            int[] top = q.poll();
+            int i = top[0],  j = top[1];
             
-            int i = curr[0], j = curr[1];
-            
-            for(int k = 0; k < 4; k++) {
+            for(int k = 0; k < 4; k++){
                 int[] d = dir[k];
-                String path = pathdp[i][j] ;
+                int r = d[0] + i;
+                int c = d[1] + j;
                 
-                int r = i + d[0];
-                int c = j + d[1];
-
-                int cnt = 0;
-                
+                String path = dpPath[i][j] + sdir[k];
                 int dist = dis[i][j];
-                path += dirs[k];
                 
-                while(r >= 0 && c >= 0 && r < n && c < m && maze[r][c] == 0) {
-
-                    if(r == hole[0] && c == hole[1]) {
-                        break;
-                    }
+                while(r >= 0 && c >= 0 && r < n && c < m && maze[r][c] == 0){
                     
-                    r += d[0]; 
-                    c += d[1];
+                    if(r == hole[0] && c == hole[1]) break;
+                    
+                    r = r + d[0];
+                    c = c + d[1];
                     dist++;
                 }
                 
                 if(r != hole[0] || c != hole[1]){
-                    r -= d[0];
-                    c -= d[1];
+                    r -= d[0]; c -= d[1];
                 }
                 
-                if(r == i && c == j) {
-                    continue;
-                }
+                if(r == i && c == j) continue;
                 
-                if(dis[r][c] >= dist){
-                    
-                   if(dis[r][c] > dist){
-                       dis[r][c] = dist;
-                       pathdp[r][c] = path;
-                   }
-                   else if(path.compareTo(pathdp[r][c]) < 0){
-                        pathdp[r][c] = path;
-                   }
-                   q.add(new int[] {r, c});
+                if(dist <= dis[r][c]){
+                    if(dist < dis[r][c]){
+                        dis[r][c] = dist;
+                        dpPath[r][c] = path;
+                    }
+                    else if(path.compareTo(dpPath[r][c]) < 0){
+                        dpPath[r][c] = path;
+                    }
+                    q.add(new int[] {r, c});
                 }
-                
             }
         }
-        return pathdp[hole[0]][hole[1]].equals("z") ? "impossible" : pathdp[hole[0]][hole[1]];
+        
+        return dpPath[hole[0]][hole[1]].equals("z") ? "impossible" : dpPath[hole[0]][hole[1]];
     }
 }
