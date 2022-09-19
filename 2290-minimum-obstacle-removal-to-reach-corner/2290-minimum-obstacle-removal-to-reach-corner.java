19deec71c0dfb0cec56faca2b0a25863f21bdd00
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-20 03:50:43 +0530

    Time: 300 ms (49.13%), Space: 130 MB (53.10%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
@@ -4,11 +4,11 @@ class Solution {
          int n = grid.length, m = grid[0].length;
         PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        int[][] vis = new int[n][m];
+        boolean[][] vis = new boolean[n][m];
         
-        for(int[] v : vis) Arrays.fill(v, Integer.MAX_VALUE);
+        // for(int[] v : vis) Arrays.fill(v, Integer.MAX_VALUE);
         
-        vis[0][0] = grid[0][0];
+        vis[0][0] = true;
         q.add(new int[]{0, 0, 0});
         
         while(!q.isEmpty()){
@@ -22,16 +22,16 @@ class Solution {
                     int r = i + d[0];
                     int c = j + d[1];
 
-                    if(r >= 0 && c >= 0 && r < n && c < m){
+                    if(r >= 0 && c >= 0 && r < n && c < m && !vis[r][c]){
                         int newk = k + grid[r][c];
-                        if(vis[r][c] > newk){
-                            vis[r][c] = newk;
+                        // if(vis[r][c] > newk){
+                            vis[r][c] = true;
                             q.add(new int[]{r, c, newk});
-                        }
+                        // }
                     }
                 }
             }
         }
-        return vis[n-1][m-1];
+        return -1;
     }
 }
