3c2ce95cb33f08d1a1ed38f22df5122d1a2ddc93
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-19 20:21:42 +0530

    Time: 1 ms (92.58%), Space: 44.4 MB (39.00%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.java
+++ 419-battleships-in-a-board/419-battleships-in-a-board.java
@@ -1,36 +1,38 @@
 class Solution {
     
-    int ii, jj;
-    //int[][] dir;
+    int row, col ;
+    int n, m;
     
     public int countBattleships(char[][] board) {
-       
-        int ans = 0;
-        int n = board.length, m = board[0].length;
-        int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
         
-        for(int i = 0; i< n; i++){
-            for(int j = 0 ; j < m; j++){
+        n = board.length; m = board[0].length;
+        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        int ans = 0;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(board[i][j] == 'X'){
-                    DFS(i, j, n, m, board, dir);
-                    //if(ii == i || jj == j) 
-                    ans++;
+                    DFS(i, j, board, dir);
+                    if(row == i || col == j){
+                        ans++;
+                    }
                 }
             }
         }
         return ans;
     }
-    private void DFS(int i, int j, int n, int m, char[][] board, int[][] dir){
+    
+    private void DFS(int i, int j, char[][] board, int[][] dir){
         
-        ii = i; jj = j;
-        board[i][j] = 'O';
+        row = i; col = j;
+        board[i][j] = '.';
         
-        for(int d = 0; d < 4; d++){
-            int r = i + dir[d][0];
-            int c = j + dir[d][1];
+        for(int[] d : dir){
+            int r = i + d[0];
+            int c = j + d[1];
             
             if(r >= 0 && c >= 0 && r < n && c < m && board[r][c] == 'X'){
-                DFS(r, c, n, m, board, dir);
+                DFS(r, c, board, dir);
             }
         }
     }
