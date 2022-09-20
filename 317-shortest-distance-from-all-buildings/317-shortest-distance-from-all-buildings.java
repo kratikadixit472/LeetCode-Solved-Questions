6d3a7890592cda5b914c2de53199a94398d07006
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-21 01:52:59 +0530

    Time: 487 ms (45.46%), Space: 117.2 MB (61.37%) - LeetHub

--- 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.java
@@ -3,57 +3,63 @@ class Solution {
         
         int n = grid.length, m = grid[0].length;
         
-        int[][] reach = new int[n][m];
-        int[][] dist = new int[n][m];
-        int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        int[][] dis = new int[n][m];
         
-        int totalHouses = 0;
+        int[][] reach = new int[n][m];
+        
+        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        
+        int totalBuildings = 0;
         
         for(int i = 0; i < n; i++){
-            for(int j = 0 ; j < m; j++){
+            for(int j = 0; j < m; j++){
+                
                 if(grid[i][j] == 1){
-                    totalHouses++;
+                    
+                    totalBuildings++;
                     
                     boolean[][] vis = new boolean[n][m];
                     LinkedList<int[]> q = new LinkedList<>();
+                    
                     q.add(new int[]{i, j});
-                    int level = 1;
+                    vis[i][j] = true;
+                    int distance = 1;
                     
                     while(!q.isEmpty()){
+                        
                         int sz = q.size();
                         while(sz-- > 0){
-                            
-                            int[] curr = q.poll();
-                            int r = curr[0], c = curr[1];
-                            
-                            for(int d [] : dir){
+                            int[] top = q.poll();
+                            int r = top[0], c = top[1];
+
+                            for(int[] d: dir){
                                 int nr = r + d[0];
                                 int nc = c + d[1];
-                                
+
                                 if(nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == 0 && !vis[nr][nc]){
-                                    dist[nr][nc] += level;
                                     reach[nr][nc]++;
-                                    q.add(new int[]{nr, nc});
+                                    dis[nr][nc] += distance;
                                     vis[nr][nc] = true;
+                                    q.add(new int[]{nr, nc});
                                 }
                             }
                         }
-                        level++;
+                        distance++;
                     }
                 }
             }
         }
         
-        int minDist = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
         
         for(int i = 0; i < n; i++){
-            for(int j = 0 ; j < m; j++){
-                if(reach[i][j] == totalHouses){
-                    minDist = Math.min(minDist, dist[i][j]);
+            for(int j = 0; j < m; j++){
+                if(reach[i][j] == totalBuildings){
+                    ans = Math.min(ans, dis[i][j]);
                 }
             }
         }
         
-        return (minDist == Integer. MAX_VALUE)  ? -1 : minDist;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
