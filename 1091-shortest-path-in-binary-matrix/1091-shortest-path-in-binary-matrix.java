c0357c49a35b956782e2ffd2c1f0815bc2ff93a5
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-20 17:04:27 +0530

    Time: 23 ms (77.06%), Space: 58.7 MB (74.13%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,36 +1,37 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         
-        if(grid[0][0] != 0 || grid[grid.length-1][grid[0].length-1] != 0) return -1;
-        
+        int n = grid.length, m = grid[0].length;
         LinkedList<int[]> q = new LinkedList<>();
+        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}, {-1, -1}, {1, 1}, {-1, 1}, {1, -1}};
         
-        int[][] dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {1, -1}, {-1, 1}, {1, 1}, {-1, -1}};
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
         
         q.add(new int[]{0, 0});
         grid[0][0] = 1;
         
-        int level = 0;
+        int distance = 0;
         
         while(!q.isEmpty()){
             int sz = q.size();
             while(sz-- > 0){
                 
-                int[] peek = q.poll();
+                int[] top = q.poll();
                 
-                if(peek[0] == grid.length-1 && peek[1] == grid[0].length-1) return level + 1;
+                int i = top[0], j = top[1];
                 
+                if(i == n-1 && j == m-1) return distance + 1;
                 for(int[] d : dir){
-                    int r = peek[0] + d[0];
-                    int c = peek[1] + d[1];
+                    int r = i + d[0];
+                    int c = j + d[1];
                     
-                    if(r >= 0 && c >= 0 && r < grid.length && c < grid[0].length && grid[r][c] == 0){
-                        q.add(new int[] {r, c});
+                    if(r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 0){
                         grid[r][c] = 1;
+                        q.add(new int[]{r, c});
                     }
                 }
             }
-            level++;
+            distance++;
         }
         return -1;
     }
