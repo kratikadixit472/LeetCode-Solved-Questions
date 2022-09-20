c9fa70d55d9d5cb20cc756a8b9225c4558d76f06
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-20 18:51:41 +0530

    Time: 35 ms (46.65%), Space: 63.4 MB (72.74%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -11,53 +11,34 @@ class Solution {
             graph[time[0]].add(new int[]{time[1], time[2]});
         }
         
-        return dikshtraAlgo(k, n, graph);
-    }
-    
-   
-    private int dikshtraAlgo(int src, int n, List<int[]>[] graph){
+        // Arrays.sort(graph, (a, b) -> a[1] - b[1]);
         
-        int res = 0, N = n;
-        int[] dis = new int[n+1];
-        boolean[] vis = new boolean[n+1];
+        int[] signal = new int[n+1];
+        Arrays.fill(signal, Integer.MAX_VALUE);
         
-        Arrays.fill(dis, (int)(1e8));
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        q.add(k);
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        signal[k] = 0;
         
-        pq.add(new int[]{src, 0});
-        dis[src] = 0;
-        
-        while(!pq.isEmpty()){
+        while(!q.isEmpty()){
+            
+            int curr = q.poll();
+            
+            for(int[] ngbr : graph[curr]){
+                int time = ngbr[1], next = ngbr[0];
                 
-            int[] top = pq.poll();
-            
-            int vtx = top[0], wsf = top[1];
-            
-            if(vis[vtx]) continue;
-            
-            vis[vtx] = true;
-            // dis[vtx] = wsf;
-            res = Math.max(wsf, res);
-            N--;
-                
-            for(int[] e : graph[vtx]){
-                int v = e[0], w = e[1];
-                if(!vis[v] && w + wsf < dis[v]) {
-                    dis[v] = w + wsf;
-                    //par[e] = vtx;
-                    pq.add(new int[]{v, w + wsf});
+                int arrivalTime = signal[curr] + time;
+                if(signal[next] > signal[curr] + time){
+                    signal[next] = signal[curr] + time;
+                    q.add(next);
                 }
             }
         }
-        
-//         int max = Integer.MIN_VALUE;
-        
-//         for(int i = 1 ; i <= n; i++){
-            
-//             max = Math.max(max, dis[i]);
-//             if(max == (int)(1e8)) return -1;
-//         }
-        return (N != 0 ) ? -1 : res;
+        int ans = 0;
+        for(int i = 1; i <= n; i++){
+            ans = Math.max(ans, signal[i]);
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
