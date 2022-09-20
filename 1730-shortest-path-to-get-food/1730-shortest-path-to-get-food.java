86112dca932133b6a177b49c08ae6312c95356a9
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-20 05:41:21 +0530

    Time: 13 ms (55.91%), Space: 62.5 MB (52.85%) - LeetHub

--- 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.java
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.java
@@ -2,44 +2,43 @@ class Solution {
     public int getFood(char[][] grid) {
         
         int n = grid.length, m = grid[0].length;
+        LinkedList<int[]> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
+        
         int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         
-        LinkedList<int[]> q = new LinkedList<>();
-        
-        for(int i = 0 ; i < n; i++){
+        for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == '*'){
                     q.add(new int[]{i, j});
+                    vis[i][j] = true;
+                    break;
                 }
             }
         }
         
-        int ans = 1;
+        int distance = 0;
         
         while(!q.isEmpty()){
+            
             int sz = q.size();
             while(sz-- > 0){
+                int[] top = q.poll();
                 
-                int[] curr = q.poll();
-                //System.out.println(curr[0]+" "+ curr[1]+" "+ grid[curr[0]][curr[1]]+", ");
+                int i = top[0], j = top[1];
+                if(grid[i][j] == '#') return distance;
                 
-                if(grid[curr[0]][curr[1]] == '#') return ans;
-                
-                for(int d[] : dir){
-                    int r = curr[0] + d[0];
-                    int c = curr[1] + d[1];
-                  // System.out.println("entered  "+ r +" "+ c+" ");
+                for(int[] d : dir){
+                    int r = i + d[0];
+                    int c = j + d[1];
                     
-                    if(r >= 0 && c >= 0 && r < n && c < m && grid[r][c] != 'X'){
-                        
-                        if(grid[r][c] == '#') return ans;
-                        
+                    if(r >= 0 && c >= 0 && r < n && c < m && !vis[r][c] && grid[r][c] != 'X'){
+                        vis[r][c] = true;
                         q.add(new int[]{r, c});
-                        grid[r][c] = 'X';
                     }
                 }
             }
-            ans++;
+            distance++;
         }
         return -1;
     }
