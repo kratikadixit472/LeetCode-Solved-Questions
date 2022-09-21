729b94a8cb33c3aac5adebd1a7227aeabe9d1282
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-21 20:27:03 +0530

    Time: 1025 ms (5.07%), Space: 68.1 MB (71.85%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,19 +1,25 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
+        
         Arrays.sort(courses, (a, b) -> a[1] - b[1]);
-       
-        int time = 0, count = 0;
-        for (int i = 0; i < courses.length; i++) {
-            if (time + courses[i][0] <= courses[i][1]) {
+        
+        int n = courses.length;
+        int count = 0, time = 0;
+        
+        for(int i = 0; i < n ; i++){
+            if(time + courses[i][0] <= courses[i][1]){
                 time += courses[i][0];
                 count++;
-            } else {
+            }
+            
+            else{
                 int max_i = i;
-                for (int j = 0; j < i; j++) {
-                    if (courses[j][0] > courses[max_i][0])
+                for(int j = 0; j < i; j++){
+                    if(courses[j][0] > courses[max_i][0]){
                         max_i = j;
+                    }
                 }
-                if (courses[max_i][0] > courses[i][0]) {
+                if(courses[i][0] < courses[max_i][0]){
                     time += courses[i][0] - courses[max_i][0];
                 }
                 courses[max_i][0] = -1;
