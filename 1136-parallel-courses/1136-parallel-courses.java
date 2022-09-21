2d3769ebaa1cb7040a5671cd034e051e9b21f8de
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-21 21:55:57 +0530

    Time: 15 ms (63.20%), Space: 53.7 MB (80.72%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.java
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -2,40 +2,37 @@ class Solution {
     public int minimumSemesters(int n, int[][] relations) {
         
         List<Integer>[] graph = new ArrayList[n+1];
+        
         for(int i = 0; i <= n; i++){
             graph[i] = new ArrayList<>();
         }
-        
         int[] indegree = new int[n+1];
         
         for(int[] rel : relations){
-            graph[rel[1]].add(rel[0]);
-            indegree[rel[0]]++;
+            graph[rel[0]].add(rel[1]);
+            indegree[rel[1]]++;
         }
+        
         LinkedList<Integer> q = new LinkedList<>();
-        
         for(int i = 1; i <= n; i++){
-            if(indegree[i] == 0) {
-                q.add(i);
-            // System.out.print(indegree[i]+ " "+i+", ");
-            }
+            if(indegree[i] == 0) q.add(i);
         }
         
-        int semeters = 0, courseCount = 0;
+        int minSem = 0;
+        
         while(!q.isEmpty()){
             int sz = q.size();
-            semeters++;
             while(sz-- > 0){
                 int top = q.poll();
-                courseCount++;
+
                 for(int next : graph[top]){
                     indegree[next]--;
-                    if(indegree[next] == 0) {
-                        q.add(next);
-                    }
+                    if(indegree[next] == 0)q.add(next);
                 }
             }
+            minSem++;
         }
-        return courseCount != n ? -1 : semeters;
+        for(int in : indegree) if(in != 0) return -1;
+        return minSem ;
     }
 }
