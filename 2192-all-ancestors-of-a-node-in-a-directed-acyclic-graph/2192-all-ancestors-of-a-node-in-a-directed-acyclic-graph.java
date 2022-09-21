f24fe9dd8dae2c601cd0069002845eb5d98d3575
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-22 03:34:52 +0530

    Time: 198 ms (60.86%), Space: 137.3 MB (40.51%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -10,37 +10,55 @@ class Solution {
         
         for(int[] e : edges){
             map.get(e[0]).add(e[1]);
-            indegree[e[1]]++;
+            // indegree[e[1]]++;
         }
         LinkedList<Integer> q = new LinkedList<>();
         
         List<List<Integer>> ans = new ArrayList<>();
-        List<Set<Integer>> sets = new ArrayList<>();
+//         List<Set<Integer>> sets = new ArrayList<>();
+        
+//         for(int i = 0; i < n; i++){
+//             sets.add(new HashSet<>());
+//             if(indegree[i] == 0) {
+//                 q.add(i);
+//             }
+//         }
+        
+//         while(!q.isEmpty()){
+//             int parent = q.poll();
+//             for(int child : map.getOrDefault(parent, new ArrayList<>())){
+//                 sets.get(child).add(parent);
+//                 sets.get(child).addAll(sets.get(parent));
+//                 if(--indegree[child] == 0) 
+//                     q.add(child);
+//             }
+//         }
+        
+//         for(Set<Integer> set : sets){
+            
+//             List<Integer> al = new ArrayList<>();
+//             for(int ele : set) al.add(ele);
+//             Collections.sort(al);
+//             ans.add(al);
+//         }
         
         for(int i = 0; i < n; i++){
-            sets.add(new HashSet<>());
-            if(indegree[i] == 0) {
-                q.add(i);
-            }
+            ans.add(new ArrayList<>());
         }
-        
-        while(!q.isEmpty()){
-            int parent = q.poll();
-            for(int child : map.getOrDefault(parent, new ArrayList<>())){
-                sets.get(child).add(parent);
-                sets.get(child).addAll(sets.get(parent));
-                if(--indegree[child] == 0) 
-                    q.add(child);
-            }
-        }
-        
-        for(Set<Integer> set : sets){
-            
-            List<Integer> al = new ArrayList<>();
-            for(int ele : set) al.add(ele);
-            Collections.sort(al);
-            ans.add(al);
+        for(int i = 0; i < n; i++){
+            DFS(i, i, ans, map);
         }
         return ans;
+    }
+    private void DFS(int par, int kid, List<List<Integer>> ans, Map<Integer, List<Integer>> map){
+        
+        List<Integer> ancestors = ans.get(kid);
+        if(ancestors.isEmpty() || ancestors.get(ancestors.size()-1) != par){
+            if(par != kid) ancestors.add(par);
+            for(int grandKid : map.get(kid)){
+                DFS(par, grandKid, ans, map);
+            }
+        }
+        
     }
 }
