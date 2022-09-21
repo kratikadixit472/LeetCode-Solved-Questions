7257c5440c7b51005f876463d4bbd9575fbe7496
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-21 12:21:48 +0530

    Time: 85 ms (59.86%), Space: 46.2 MB (89.35%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -2,39 +2,38 @@ class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
         
         List<String> ans = new ArrayList<>();
-        
         Map<String, List<String>> graph = new HashMap<>();
-        Map<String, Integer> indegree = new HashMap<>();
+        Map<String , Integer> indegree = new HashMap<>();
         
         for(int i = 0; i < ingredients.size(); i++){
-            for(String ingred : ingredients.get(i)){
-                // System.out.print(ingred+" "+recipes[i]+",  ");
-                graph.putIfAbsent(ingred, new ArrayList<>());
+            List<String> al = ingredients.get(i);
+            for(String ingred : al){
+                graph.putIfAbsent(ingred, new ArrayList<>()); 
                 graph.get(ingred).add(recipes[i]);
-                indegree.put(recipes[i], indegree.getOrDefault(recipes[i], 0) + 1);
+                indegree.put(recipes[i], indegree.getOrDefault(recipes[i], 0)+1);
             }
         }
         
         LinkedList<String> q = new LinkedList<>();
-        for(String supp : supplies){
-            q.add(supp);
+        
+        for(String supply : supplies){
+            q.add(supply);
         }
         
         while(!q.isEmpty()){
             
-            String curr = q.poll();
-            // System.out.print("." + curr+", ");
-            if(graph.get(curr) == null) continue;
-            for(String recp : graph.get(curr)){
-                indegree.put(recp, indegree.get(recp) - 1);
-                if(indegree.get(recp) == 0) q.add(recp);
+            String top = q.poll();
+            if(graph.get(top) == null) continue;
+            
+            for(String rcp : graph.get(top)){
+                indegree.put(rcp, indegree.getOrDefault(rcp, 0) - 1);
+                if(indegree.get(rcp) == 0) q.add(rcp);
             }
         }
         
-        for(String recp : recipes){
-            if(indegree.get(recp) == 0) ans.add(recp);
+        for(String rcp : indegree.keySet()){
+            if(indegree.get(rcp) == 0) ans.add(rcp);
         }
-        
         return ans;
     }
 }
