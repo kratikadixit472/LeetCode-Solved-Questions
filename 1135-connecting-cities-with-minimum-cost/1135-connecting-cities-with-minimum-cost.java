56ea4f1522ee5299e540e69e51336ccb73d8966f
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-22 22:03:38 +0530

    Time: 55 ms (43.62%), Space: 73.2 MB (20.29%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.java
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.java
@@ -1,36 +1,31 @@
 class Solution {
     
     int[] par;
-    
     private int findPar(int u){
-        
         return (par[u] == u) ? u : (par[u] = findPar(par[u]));
     }
     
     public int minimumCost(int n, int[][] connections) {
         
-        int ans = 0;
+        par = new int[n+1];
         
-        Arrays.sort(connections, (a, b) -> {
-            return a[2] - b[2];
-        });
-        
-        par = new int[n + 1];
-        
-        for(int i = 0; i <= n; i++){
+        for(int i = 0; i < n; i++){
             par[i] = i;
         }
         
-        for(int edges[] : connections){
-            int x = edges[0], y = edges[1], w = edges[2];
-            int p1 =findPar(x), p2 = findPar(y);
+        Arrays.sort(connections, (a, b) -> a[2] - b[2]);
+        int minCost = 0;
+        
+        for(int[] c : connections){
+            int i = c[0], j = c[1], cost = c[2];
+            
+            int p1 = findPar(i), p2 = findPar(j);
             if(p1 != p2){
-                par[p2] = p1;
-                ans += w;
+                par[p1] = p2;
+                minCost += cost;
                 n--;
             }
         }
-        
-        return (n > 1) ? -1 : ans;
+        return (n > 1) ? -1 : minCost;
     }
 }
