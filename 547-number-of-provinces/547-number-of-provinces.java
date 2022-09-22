64e8a9850e00880035098d0c40778018fe8661a2
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-22 19:33:58 +0530

    Time: 3 ms (52.01%), Space: 53.7 MB (68.98%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,30 +1,32 @@
 class Solution {
+    
     int[] par;
+    private int findPar(int u){
+        return (par[u] == u) ? u : (par[u] = findPar(par[u]));
+    }
     
     public int findCircleNum(int[][] isConnected) {
         
-        int n = isConnected.length;
-        int provinces = n;
+        int n = isConnected.length, m = isConnected[0].length;
         
-        par = new int[n];
+        par = new int[n+1];
+        for(int i = 0; i <= n ; i++){
+            par[i] = i;
+        }
+        int connected = n;
         
-        for(int i = 0; i < n; i++) par[i] = i;
-        
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i+1 ; j < n; j++){
-                if(i != j && isConnected[i][j] == 1){
-                    int p1 = findPar(i), p2 = findPar(j);
-                    if(p1 != p2){
-                        par[p1] = p2;
-                        provinces--;
-                    }
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < m; j++){
+                if(isConnected[i][j] == 0) continue;
+                
+                int p1 = findPar(i+1), p2 = findPar(j+1);
+                
+                if(p1 != p2){
+                    par[p1] = par[p2];
+                    connected--;
                 }
             }
         }
-        return provinces;
-    }
-    
-    private int findPar(int u){
-        return (par[u] == u) ? u : (par[u] = findPar(par[u]));
+        return connected;
     }
 }
