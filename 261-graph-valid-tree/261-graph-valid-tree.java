ed457a8394923ec04b72df50b6f32541b89740c8
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-23 04:30:24 +0530

    Time: 12 ms (17.57%), Space: 46.8 MB (47.26%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,43 +1,42 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
+        
         List<Integer>[] graph = new ArrayList[n];
         
         for(int i = 0; i < n; i++){
             graph[i] = new ArrayList<>();
         }
         
-        for(int[] pre : edges){
-            graph[pre[0]].add(pre[1]);
-            graph[pre[1]].add(pre[0]);
+        for(int[] e : edges){
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
         }
         
-        int vis[] = new int[n];
+        int[] vis = new int[n];
         Arrays.fill(vis, -1);
         int cnt = 0;
         
-        for(int i = 0; i < n; i++){
-             if(vis[i] == -1){
-                 if(cnt == 1) return false; ;
-                 cnt++;
-                if(isCycle(0, graph, vis)) return false;
-             } 
+        for(int i = 0 ; i < n; i++){
+            if(vis[i] == -1){
+                if(cnt == 1) return false;
+                cnt++;
+                if(DFS(i, graph, vis)) return false;
+            }
         }
         return true;
     }
-    
-    private boolean isCycle(int src, List<Integer>[] graph, int[] vis){
+    private boolean DFS(int src, List<Integer>[] graph, int[] vis){
         
-        vis[src] = 0;
+        // if(vis[src] == 1) return true;
+        vis[src] = 1;
         
         for(int e : graph[src]){
             if(vis[e] == -1){
-                if(isCycle(e, graph, vis)) return true;
+                if(DFS(e, graph, vis)) return true;
             }
-            else if(vis[e] == 1) return true;
+            else if(vis[e] == 2)return true;
         }
-        
-        vis[src] = 1;
+        vis[src] = 2;
         return false;
-    
     }
 }
