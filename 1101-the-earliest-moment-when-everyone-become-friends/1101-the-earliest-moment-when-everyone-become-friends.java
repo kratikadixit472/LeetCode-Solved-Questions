521fb1162d199a417f4b0e99a8ecf528389fb905
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-22 19:10:26 +0530

    Time: 12 ms (32.76%), Space: 47.7 MB (37.58%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.java
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.java
@@ -1,36 +1,31 @@
 class Solution {
     
     int[] par;
-    
     private int findPar(int u){
         return (par[u] == u) ? u : (par[u] = findPar(par[u]));
     }
     
     public int earliestAcq(int[][] logs, int n) {
+        par = new int[n];
         
         Arrays.sort(logs, (a, b) -> a[0] - b[0]);
         
-        int groupCount = n;
-        
-        par = new int[n];
-        
-        for(int i = 0;  i < n; i++){
+        for(int i = 0; i < n; i++){
             par[i] = i;
         }
         
-        int earliestTime = 0;
+        
         
         for(int[] log : logs){
+            int time = log[0], x = log[1], y = log[2];
             int p1 = findPar(log[1]), p2 = findPar(log[2]);
-            int time = log[0];
             
             if(p1 != p2){
-                par[p2] = par[p1];
-                groupCount--;
+                par[p1] = par[p2];
+                n--;
             }
-            if(groupCount == 1) return time;
+            if(n == 1) return time;
         }
-        
         return -1;
     }
 }
