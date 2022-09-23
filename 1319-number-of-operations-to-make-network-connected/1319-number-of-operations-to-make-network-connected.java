38cf477ee39a59adad9496de894047c2e2b3a35a
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-23 20:38:01 +0530

    Time: 6 ms (86.87%), Space: 64.1 MB (69.15%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,6 +1,37 @@
 class Solution {
-    int count = 0;
+    
+    // 1. union find
+    
+    int[] par, size;
     public int makeConnected(int n, int[][] connections) {
+        
+        if(connections.length < n-1) return -1;
+        
+        par = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            par[i] = i;
+        }
+        
+        int componenet = n;
+        
+        for(int[] e : connections){
+            int p1 = findPar(e[0]), p2 = findPar(e[1]);
+            if(p1 != p2){
+                par[p2] = p1;
+                componenet--;
+            }
+        }
+        return componenet-1;
+    }
+    private int findPar(int u){
+        return (par[u] == u) ? u : (par[u] = findPar(par[u]));
+    }
+    
+    
+    // 2. DFS Connected components
+    
+    /*public int makeConnected(int n, int[][] connections) {
         
         if(connections.length < n-1) return -1;
         
@@ -36,5 +67,5 @@ class Solution {
         for(int e : graph[src]){
             if(!vis[e]) DFS(e, graph, vis );
         }
-    }
+    }*/
 }
