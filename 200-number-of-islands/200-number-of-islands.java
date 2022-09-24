9d8969d920b7a3cb6e3a4054bb903cc509fae30e
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-24 21:32:52 +0530

    Time: 4 ms (77.90%), Space: 57.9 MB (26.51%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -2,34 +2,37 @@ class Solution {
     public int numIslands(char[][] grid) {
         
         int n = grid.length, m = grid[0].length;
+        int[][] dir = {{-1, 0},{1, 0}, {0,-1}, {0, 1}};
         
-        int[][] dir = {{-1,0},{0,-1},{1,0},{0,1}};
-        int island = 0;
+        int numOfIsland = 0;
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == '1'){
-                    DFS(i, j, n, m, grid, dir);
-                    island++;
+                    islands_traversal(i, j, n, m, grid, dir);
+                    numOfIsland++;
                 }
             }
         }
-        
-        return island;
+        return numOfIsland;
     }
     
-    private void DFS(int sr, int sc, int er, int ec, char[][] grid, int[][] dir){
+    private void islands_traversal(int r, int c, int n, int m, char[][] grid, int[][] dir){
         
-        grid[sr][sc] = '0';
+        grid[r][c] = '0';
         
         for(int[] d : dir){
+            int nr = r + d[0];
+            int nc = c + d[1];
             
-            int r = sr + d[0];
-            int c = sc + d[1];
-            
-            if(r >= 0 && c >= 0 && r<er && c<ec && grid[r][c] == '1'){
-                DFS(r, c, er, ec, grid, dir);
+            if(isValid(nr, nc, n, m, grid)){
+                islands_traversal(nr, nc, n, m, grid, dir);
             }
         }
+    }
+    
+    private boolean isValid(int r, int c, int n, int m, char[][] grid){
+        if(r < 0 || c < 0 || r >= n || c >= m || grid[r][c] == '0') return false;
+        return true;
     }
 }
