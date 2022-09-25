167f3691018730cd6c6a253b57659723d0e69937
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-26 00:02:36 +0530

    Time: 9 ms (34.06%), Space: 42 MB (61.39%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.java
@@ -1,23 +1,30 @@
 class Solution {
     public int shortestWay(String source, String target) {
         
-        int[] frq = new int[26];
-        int res = 1;
-        for(int i = 0; i < source.length(); i++){
-            frq[source.charAt(i) - 'a']++;
+        Map<Character, Boolean> map = new HashMap<>();
+        
+        char[] s = source.toCharArray();
+        char[] t = target.toCharArray();
+        
+        for(char c : s){
+            map.put(c, true);
         }
-        for(int i = 0, j = 0; i < target.length(); i++, j++){
-            if(frq[target.charAt(i) - 'a'] == 0) return -1;
+        
+        int ans = 1, j = 0;
+        
+        for(int i = 0; i < target.length(); i++, j++){
+            if(!map.getOrDefault(t[i], false)) return -1;
             
-            while(j < source.length() && source.charAt(j) != target.charAt(i)) j++;
+            while(j < s.length && s[j] != t[i]){
+                j++;
+            }
             
             if(j == source.length()){
                 j = -1;
                 i -= 1;
-                res++;
+                ans++;
             }
         }
-        
-        return res;
+        return ans;
     }
 }
