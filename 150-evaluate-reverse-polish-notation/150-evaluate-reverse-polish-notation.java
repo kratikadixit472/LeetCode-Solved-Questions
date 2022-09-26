bd722b54bd008c664487e319452c32499d5367a1
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-26 18:57:51 +0530

    Time: 16 ms (20.65%), Space: 44.6 MB (23.03%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,28 +1,34 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         
+        int n = tokens.length;
         Stack<Integer> st = new Stack<>();
         
-        for(String s : tokens){
-            if(!s.equals("+") && !s.equals("*") && !s.equals("-") && !s.equals("/")){
-                st.push(Integer.valueOf(s));
+        for(int i = 0; i < n; i++){
+            
+            if((!tokens[i].equals("-") && !tokens[i].equals("*") && !tokens[i].equals("+") && !tokens[i].equals("/"))){
+                st.push(Integer.valueOf(tokens[i]));
             }
-            else{
+            
+            else if(st.size() > 1){
                 int b = st.pop();
                 int a = st.pop();
-                if(s.equals("+")){
-                    int c = a + b;
-                    st.push((c));
-                }else if(s.equals("*")){
-                    int c = a * b;
-                    st.push((c));
-                }else if(s.equals("/")){
-                    int c = a / b;
-                    st.push((c));
-                }else if(s.equals("-")){
-                    int c = a - b;
-                    st.push((c));
+                
+                int res = 0;
+                
+                if(tokens[i].equals("+")) {
+                    res = a + b;
                 }
+                else if(tokens[i].equals("-")) {
+                    res = (a - b);
+                }
+                else if(tokens[i].equals("*")){
+                    res = (a * b);
+                }
+                else if(tokens[i].equals("/")) {
+                    res = (a / b);
+                }
+                st.push(res);
             }
             
         }
