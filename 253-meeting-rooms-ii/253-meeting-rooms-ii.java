a5555fc9788e3277cff5c8334af102a29e1e30ae
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-27 20:02:23 +0530

    Time: 5 ms (92.47%), Space: 45 MB (60.17%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,27 +1,29 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-       
         
-        int[] start = new int[intervals.length];
-        int[] end = new int[intervals.length];
+        int n = intervals.length;
+        
+        int[] start = new int[n];
+        int[] end = new int[n];
+        
+        for(int i =0 ; i < n; i++){
+            int[] interval = intervals[i];
+            start[i] = interval[0];
+            end[i] = interval[1];
+        }
+        
+        Arrays.sort(start); Arrays.sort(end);
+        
+        int rooms = 0, endIdx = 0;
         
         for(int i = 0; i < intervals.length; i++){
-             start[i] = intervals[i][0];
-             end[i] = intervals[i][1];
+            if(start[i] < end[endIdx]){
+                rooms++;
+            }
+            else{
+                endIdx++;
+            }
         }
-        
-        Arrays.sort(start);
-        Arrays.sort(end);
-        
-        int[] interval = intervals[0];
-        int ans = 1, idx = 0;
-        
-        for(int i = 1; i < intervals.length; i++){
-            
-            if(start[i] < end[idx]) ans++;
-            
-            else idx++;
-        }
-        return ans;
+        return rooms;
     }
 }
