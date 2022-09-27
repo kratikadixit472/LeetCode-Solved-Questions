a8ed78628989e62d07b2d4fb34d43a672960e859
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-27 19:37:29 +0530

    Time: 432 ms (71.21%), Space: 190.7 MB (18.74%) - LeetHub

--- 1882-process-tasks-using-servers/1882-process-tasks-using-servers.java
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.java
@@ -1,55 +1,43 @@
 class Solution {
-    
-    class Pair{
-        int w, id;
-        int nextSec;
-        
-        public Pair(int id, int w){
-            this.id = id;
-            this.w = w;
-        }
-    }
-    
     public int[] assignTasks(int[] servers, int[] tasks) {
         
-        PriorityQueue<int[]> freeServer = new PriorityQueue<>((a, b) -> (a[0] != b[0]) ? (a[0] - b[0]) : (a[1] - b[1]));
+        PriorityQueue<int[]> serverq = new PriorityQueue<>((a, b) -> (a[0] != b[0]) ? a[0] - b[0] : a[1] - b[1]);
         
-        PriorityQueue<int[]> filledServer = new PriorityQueue<>((a, b) -> (a[2] != b[2]) ? (a[2] - b[2]) : ((a[0] == b[0]) ? (a[1] - b[1]) : (a[0] - b[0])));
-        
+        PriorityQueue<int[]> taskq = new PriorityQueue<>((a, b) -> a[2] != b[2] ? a[2] - b[2] : (a[0] != b[0]) ? a[0] - b[0] : a[1] - b[1]);
         
         for(int i = 0; i < servers.length; i++){
-            freeServer.add(new int[]{servers[i], i, 0});
+            int server = servers[i];
+            serverq.add(new int[]{server, i, 0});
         }
         
-        int[] ans = new int[tasks.length];
+        int n = tasks.length;
+        int[] ans = new int[n];
         
-        for(int i = 0;  i < tasks.length; i++){
+        for(int i = 0; i < n ; i++){
             
-            int task = tasks[i];
-            
-            while(!filledServer.isEmpty() && filledServer.peek()[2] <= i){
-               
-                freeServer.add(filledServer.poll());
+            while(!taskq.isEmpty() && taskq.peek()[2] <= i){
+                
+                serverq.offer(taskq.poll());
+
+            }
+            if(serverq.isEmpty()) {
+                
+                int[] curr = taskq.poll();
+                ans[i] = curr[1];
+                curr[2] += tasks[i];
+                taskq.add(curr);
             }
             
-            if(freeServer.isEmpty()){
+            else {
                 
-                int[] curr = filledServer.poll();
-                ans[i] = curr[1];
-                curr[2] += task;
-                filledServer.add(curr);
+                int nextSec = i + tasks[i];
                 
-            }
-             else{
-                 
-                int nextSec = i + task;
+                int[] freeServer = serverq.poll();
+                ans[i] = freeServer[1];
+                freeServer[2] = nextSec;
                 
-                int[] curr = freeServer.poll();
-                ans[i] = curr[1];
-                curr[2] = nextSec;
-                
-                filledServer.add(curr);
-                
+                taskq.add(freeServer);
+
             }
         }
         return ans;
