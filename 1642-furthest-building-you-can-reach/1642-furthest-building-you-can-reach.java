42a33f52eee357f8d359d4458fa73164d1443f40
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-29 02:58:23 +0530

    Time: 760 ms (5.05%), Space: 120 MB (5.58%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,22 +1,38 @@
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int n = heights.length;
+        int n = heights.length, ans = -1;
         
-        for(int i = 0; i < n-1; i++){
-            int climb = heights[i+1] - heights[i];
-            
-            if(climb <= 0) continue;
-            
-            pq.add(climb);
-            
-            if(pq.size() <= ladders) continue;
-            else{
-                bricks -= pq.poll();
+        int l = 0, r = n-1;
+        while(l <= r){
+            int mid = (r + l) / 2;
+            if(check(heights, bricks, ladders, mid)){
+                ans = mid;
+                l = mid + 1;
             }
-            if(bricks < 0) return i;
+            else{
+                r = mid - 1;
+            }
         }
-        return n-1;
+        return ans;
+    }
+    
+    private boolean check(int[] heights, int bricks, int ladder, int mid){
+        
+        List<Integer> al = new ArrayList<>();
+        
+        for(int i = 1; i <= mid; i++){
+            if(heights[i] < heights[i-1]) continue;
+            int climb = heights[i] - heights[i-1];
+            al.add(climb);
+        }
+        Collections.sort(al, Collections.reverseOrder());
+        int needed = 0;
+        
+        for(int i = ladder; i < al.size(); i++){  
+            needed += al.get(i);
+            if(bricks < needed) return false;
+        }
+        return true;
     }
 }
