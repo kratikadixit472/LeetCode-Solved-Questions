9d1db49d8dee131142fb9a43f15a71e7b83a5f52
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-28 05:52:14 +0530

    Time: 1 ms (56.57%), Space: 41.2 MB (27.48%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,25 +1,24 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         
-        return findAllPossible(0, 0, m-1, n-1, new int[m][n]);
+        return DFS(0, 0, m-1, n-1, new Integer[m][n]);
     }
     
-    private int findAllPossible(int sr, int sc, int er, int ec, int[][] dp){
+    private int DFS(int sr, int sc, int er, int ec, Integer[][] dp){
         
-        if(sr == er && sc == ec) return 1;
+        if(sr == er && sc == ec) return dp[sr][sc] = 1;
         
-        if(dp[sr][sc] != 0) return dp[sr][sc];
+        if(dp[sr][sc] != null) return dp[sr][sc];
         
-        int count = 0;
+        int count = 0 ;
         
         if(sr + 1 <= er){
-            count += findAllPossible(sr + 1, sc, er, ec, dp);
+            count += DFS(sr + 1, sc, er, ec, dp);
         }
         if(sc + 1 <= ec){
-            count += findAllPossible(sr, sc + 1, er, ec, dp);
+            count += DFS(sr, sc + 1, er, ec, dp);
         }
         dp[sr][sc] = count;
-        
         return dp[sr][sc];
     }
 }
