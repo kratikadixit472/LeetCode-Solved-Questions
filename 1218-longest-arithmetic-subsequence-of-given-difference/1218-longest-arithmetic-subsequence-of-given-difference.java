0c96131351baa2ac8497f7fbde7e7194ac38915d
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-28 23:15:20 +0530

    Time: 97 ms (33.81%), Space: 90.4 MB (5.16%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.java
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.java
@@ -3,20 +3,24 @@ class Solution {
         
         int n = arr.length, max = 1;
         Map<Integer, Integer> map = new HashMap<>();
-        int[] dp = new int[n];
-        map.put(arr[n-1], 1);
-        dp[n-1] = 1;
-        // Arrays.fill(dp, 1);
+        // int[] dp = new int[n];
+        // map.put(arr[n-1], 1);
+        // dp[n-1] = 1;
         
-        for(int i = n-2; i>=0; i--){
-            int oldVal = map.getOrDefault(arr[i], 0);
-            // if(map.containsKey(arr[i] + difference)){
-                int newVal = 1 + map.getOrDefault(arr[i] + difference, 0);
-                dp[i] = Math.max(oldVal, newVal);
-                map.put(arr[i], dp[i]);
-            // }
-            max = Math.max(max, dp[i]);
+        // for(int i = n-2; i>=0; i--){
+        //     int oldVal = map.getOrDefault(arr[i], 0);
+        //         int newVal = 1 + map.getOrDefault(arr[i] + difference, 0);
+        //         dp[i] = Math.max(oldVal, newVal);
+        //         map.put(arr[i], dp[i]);
+        //     max = Math.max(max, dp[i]);
+        // }
+        
+        for(int a : arr){
+            int aVal = map.getOrDefault(a - difference, 0);
+            map.put(a, aVal+1);
+            max = Math.max(max, map.get(a));
         }
+        
         return max;
     }
 }
