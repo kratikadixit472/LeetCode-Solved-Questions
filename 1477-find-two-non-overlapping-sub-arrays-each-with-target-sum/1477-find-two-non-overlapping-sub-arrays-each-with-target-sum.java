8afcd6de318e2f1a646cd7d9b9e4f7d188daff9c
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-29 04:38:33 +0530

    Time: 103 ms (40.85%), Space: 63.7 MB (84.15%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,29 +1,29 @@
 class Solution {
     public int minSumOfLengths(int[] arr, int target) {
         
-        int sum = 0, lsum = Integer.MAX_VALUE, result = Integer.MAX_VALUE;
+        int lsum = Integer.MAX_VALUE;
+        int sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
-        int n = arr.length;
         
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < arr.length; i++){
             sum += arr[i];
             map.put(sum, i);
         }
+        int result = Integer.MAX_VALUE;
         
         sum = 0;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < arr.length; i++){
             sum += arr[i];
             
             if(map.containsKey(sum - target)){
                 lsum = Math.min(lsum, i - map.get(sum - target));
             }
             
-            if(map.containsKey(sum + target) && lsum < Integer.MAX_VALUE){
-                result = Math.min(result, map.get(sum + target) - i + lsum);
+            if(lsum < Integer.MAX_VALUE && map.containsKey(target + sum)){
+                result = Math.min(result, map.get(target + sum) - i + lsum);
             }
         }
-        
-        return (result == Integer.MAX_VALUE) ? -1 : result;
+        return result == Integer.MAX_VALUE ? -1 : result;
     }
 }
