f1278da416b90ef6ebfe72b0ddf3854b3d319b9e
Kratika Dixit <kratikadixit472@gmail.com>
2022-09-30 23:41:25 +0530

    Time: 1 ms (89.11%), Space: 42.5 MB (37.29%) - LeetHub

---
+++ 68-text-justification/68-text-justification.java
@@ -0,0 +1,57 @@
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        
+        List<String> ans = new ArrayList<>();
+        
+        int index = 0, n = words.length;
+        
+        while(index < n){
+            
+            int last = index + 1;
+            int count = words[index].length();
+            
+            while(last < n){
+                if(count + 1 + words[last].length() <= maxWidth){
+                    count += 1 + words[last].length();
+                    last++;
+                }
+                else break;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(words[index]);
+            
+            int diff = last - index - 1;
+            if(last == n || diff == 0){
+                
+                for(int i = index+1; i < last; i++){
+                    sb.append(" ");
+                    sb.append(words[i]);
+                }
+  
+                for(int i = sb.length(); i<maxWidth; i++){
+                    sb.append(" ");
+                }
+            }
+            else{
+                int spaces = (maxWidth - count) / diff;
+                int odd = (maxWidth - count) % diff;
+                
+                for(int i = index+1; i < last; i++){
+                    for(int k = spaces; k > 0; k--){
+                        sb.append(" ");
+                    }
+                    if(odd > 0){
+                        sb.append(" ");
+                        odd--;
+                    }
+                    sb.append(" ");
+                    sb.append(words[i]);
+                }
+            }
+            ans.add(sb.toString());
+            index = last;
+        }
+        return ans;
+    }
+}
