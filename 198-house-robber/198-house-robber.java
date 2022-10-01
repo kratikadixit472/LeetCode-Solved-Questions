3fb75bd44e7114ee0eda38caa537c8c1107e2ff6
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-02 05:05:14 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (44.73%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,18 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
         
-        int n = nums.length;
-        if(n == 1) return nums[0];
+        return findMaxMoney(0, nums, new Integer[nums.length]);
+    }
+    private int findMaxMoney(int idx, int[] nums, Integer[] dp){
         
-        int[] dp = new int[n];
+        if(idx >= nums.length) return 0;
         
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        if(dp[idx] != null) return dp[idx];
         
-        for(int i = 2; i < n; i++){
-            
-            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
-        }
-        return dp[n-1];
+        int takeIt = nums[idx] + findMaxMoney(idx+2, nums, dp);
+        int notTakeIt = findMaxMoney(idx+1, nums, dp);
+        
+        return dp[idx] = Math.max(takeIt, notTakeIt);
     }
 }
