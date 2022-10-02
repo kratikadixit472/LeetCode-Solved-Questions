dd620623bc177415472e1b361a5ff97153062a8b
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-02 05:48:49 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (48.71%) - LeetHub

--- 276-paint-fence/276-paint-fence.java
+++ 276-paint-fence/276-paint-fence.java
@@ -1,12 +1,14 @@
 class Solution {
     public int numWays(int n, int k) {
-        return findWaysToPaint(n, k);
+        return findWaysToPaint(n, k, new Integer[n+1]);
     }
-    private int findWaysToPaint(int n, int k){
+    private int findWaysToPaint(int n, int k, Integer[] dp){
         
         if(n == 1) return k;
         if(n == 2) return k * k;
         
-        return (k-1) * (findWaysToPaint(n-1, k) + findWaysToPaint(n-2, k));
+        if(dp[n] != null) return dp[n];
+        
+        return dp[n] = (k-1) * (findWaysToPaint(n-1, k, dp) + findWaysToPaint(n-2, k, dp));
     }
 }
