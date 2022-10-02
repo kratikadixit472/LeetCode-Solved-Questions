0ba6ab4e856472921aa3cfac9a3790a7e0805910
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-02 20:56:13 +0530

    Time: 9 ms (77.33%), Space: 63.3 MB (86.99%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,71 +1,32 @@
 class Solution {
     
-    // 1. union find
+    int[] par;
     
-    int[] par, size;
     public int makeConnected(int n, int[][] connections) {
         
-        if(connections.length < n-1) return -1;
+        if(n-1 > connections.length) return -1;
         
         par = new int[n];
         
         for(int i = 0; i < n; i++){
             par[i] = i;
         }
+        int components = n;
         
-        int componenet = n;
-        
-        for(int[] e : connections){
-            int p1 = findPar(e[0]), p2 = findPar(e[1]);
+        for(int[] c : connections){
+            int p1 = findPar(c[0]), p2 = findPar(c[1]);
+            
             if(p1 != p2){
-                par[p2] = p1;
-                componenet--;
+                par[p1] = p2;
+                components--;
             }
         }
-        return componenet-1;
+        return components - 1;
     }
+    
     private int findPar(int u){
+        
         return (par[u] == u) ? u : (par[u] = findPar(par[u]));
+        
     }
-    
-    
-    // 2. DFS Connected components
-    
-    /*public int makeConnected(int n, int[][] connections) {
-        
-        if(connections.length < n-1) return -1;
-        
-        List<Integer>[] graph = new ArrayList[n];
-        
-        for(int i = 0; i < n; i++){
-            graph[i] = new ArrayList<>();
-        }
-        
-        for(int[] conn : connections){
-            graph[conn[0]].add(conn[1]);
-            graph[conn[1]].add(conn[0]);
-        }
-        
-        int ans = 0;
-        boolean[] vis = new boolean[n];
-        
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
-        
-                DFS(i, graph, vis );
-                ans++;
-            }
-        }
-        
-        return ans-1;
-    }
-    
-    private void DFS(int src, List<Integer>[] graph, boolean[] vis ){
-        
-        vis[src] = true;
-        
-        for(int e : graph[src]){
-            if(!vis[e]) DFS(e, graph, vis );
-        }
-    }*/
 }
