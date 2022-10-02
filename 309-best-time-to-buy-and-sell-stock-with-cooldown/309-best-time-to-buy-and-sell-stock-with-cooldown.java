212d821eb1425936e3ec2fb27f9c378c3023b7f8
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-02 20:10:10 +0530

    Time: 2 ms (59.21%), Space: 41.9 MB (71.71%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -7,12 +7,11 @@ class Solution {
         int n = prices.length;
         
         for(int idx = n-1; idx >= 0; idx--){
-            for(int buy = 0; buy <= 1; buy++){
-                if(buy == 1){
-                    dp[idx][buy] = Math.max(-prices[idx] + dp[idx+1][0], 0 + dp[idx+1][1]);
-                }
-                else dp[idx][buy] = Math.max(prices[idx] + dp[idx+2][1], 0 + dp[idx+1][0]);
-            }
+            
+            dp[idx][1] = Math.max(-prices[idx] + dp[idx+1][0], 0 + dp[idx+1][1]);
+                
+            dp[idx][0] = Math.max(prices[idx] + dp[idx+2][1], 0 + dp[idx+1][0]);
+            
         }
         return dp[0][1];
     }
