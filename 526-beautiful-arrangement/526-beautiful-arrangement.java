926deb59154282b91bdbda0d5a206907283f82e3
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-02 23:27:52 +0530

    Time: 93 ms (50.00%), Space: 40 MB (86.58%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.java
+++ 526-beautiful-arrangement/526-beautiful-arrangement.java
@@ -1,26 +1,27 @@
 class Solution {
     
-    int count = 0;
+    int result = 0;
     
     public int countArrangement(int n) {
         boolean[] vis = new boolean[n+1];
-        calculate(1, vis, n);
-        return count;
+        countBeautiful(1, n, vis);
+        return result;
     }
-    
-    private void calculate(int pos, boolean[] vis, int n){
+    private void countBeautiful(int pos, int n, boolean[] vis){
         
-        if(pos == n + 1){
-            count++;
+        if(pos > n){
+            result++;
             return;
         }
         
         for(int i = 1; i <= n; i++){
-            if(!vis[i] && (i % pos == 0 || pos % i == 0)){
+            
+            if( !vis[i] && (i % pos == 0 || pos % i == 0)){
                 vis[i] = true;
-                calculate(pos+1, vis, n);
+                countBeautiful(pos + 1, n, vis);
                 vis[i] = false;
             }
+            
         }
     }
 }
