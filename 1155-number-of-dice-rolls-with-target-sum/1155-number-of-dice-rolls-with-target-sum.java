d91fd6010e9f4fe16590ea1920edfe1243f6597f
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-03 03:03:07 +0530

    Time: 17 ms (88.92%), Space: 41.2 MB (92.64%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,7 +1,12 @@
 class Solution {
     
-    int c = (int)(1e9 + 7);
-    public int numRollsToTarget(int n, int K, int target) {
+    int mod = (int)(1e9 + 7);
+    
+    public int numRollsToTarget(int n, int k, int target) {
+        return getTargetSum(0, 0, n, k, target);
+    }
+    
+    private int getTargetSum(int dice, int sum, int n, int K, int target){
         
         int[][] dp = new int[n+1][target+1];
         
@@ -14,7 +19,7 @@ class Solution {
                     
                     for(int k = 1; k <= K; k++){
                         if(k <= j){
-                            dp[i][j] = (dp[i][j] + dp[i-1][j-k]) % c;
+                            dp[i][j] = (dp[i][j] + dp[i-1][j-k]) % mod;
                         }
                     }
                 }
