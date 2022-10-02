7349c36e75667f71243720d71a88e627cae74fb3
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-02 12:38:02 +0530

    Time: 5 ms (54.94%), Space: 45.8 MB (69.59%) - LeetHub

--- 265-paint-house-ii/265-paint-house-ii.java
+++ 265-paint-house-ii/265-paint-house-ii.java
@@ -1,35 +1,30 @@
 class Solution {
     public int minCostII(int[][] costs) {
         
+        return getMinCost(0, 0, costs);
+        
+    }
+    
+    private int getMinCost(int row, int col, int[][] costs){
+        
         int n = costs.length, m = costs[0].length;
-        int[][] dp = new int[n][m];
-        
-        int ans = Integer.MAX_VALUE;
-        
-        for(int i = 0; i < m; i++){
-            dp[0][i] = costs[0][i];
-        }
         
         for(int i = 1; i < n; i++){
             for(int j = 0; j < m; j++){
-                
                 int min = Integer.MAX_VALUE;
-                
                 for(int k = 0; k < m; k++){
-                    if(j != k){
-                        min = Math.min(min, dp[i-1][k]);
-                    }
+                    
+                    if(j == k) continue;
+                    min = Math.min(min, costs[i-1][k]);
                 }
-                
-                dp[i][j] = min + costs[i][j];
+                costs[i][j] += min;
             }
-            
         }
+        int totalMinCost = Integer.MAX_VALUE;
         
-        for(int k = 0; k < m; k++){
-            ans = Math.min(dp[n-1][k], ans);
+        for(int i = 0; i < m; i++){
+            totalMinCost = Math.min(totalMinCost, costs[n-1][i]);
         }
-        
-        return ans;
+        return totalMinCost;
     }
 }
