0bc1406526d22bd7458cdab6d6945f8b7ec378fe
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-02 12:31:49 +0530

    Time: 2 ms (52.41%), Space: 43.8 MB (20.65%) - LeetHub

--- 256-paint-house/256-paint-house.java
+++ 256-paint-house/256-paint-house.java
@@ -1,35 +1,35 @@
 class Solution {
     public int minCost(int[][] costs) {
         
-        int n = costs.length;
-        int[][] dp = new int[n][3];
+        return getMinCost(0, 0, costs);
         
-        int ans = Integer.MAX_VALUE;
+    }
+    
+    private int getMinCost(int row, int col, int[][] costs){
         
-        for(int i = 0; i < 3; i++){
+        int n = costs.length, m = costs[0].length;
+        
+        int[][] dp = new int[n][m];
+        
+        for(int i = 0; i < m; i++){
             dp[0][i] = costs[0][i];
         }
         
         for(int i = 1; i < n; i++){
-            for(int j = 0; j < 3; j++){
-                
-                if(j == 0) {
-                    dp[i][j] = costs[i][j] + Math.min(dp[i-1][j+1], dp[i-1][j+2]); 
-                }
-                else if(j == 1) {
-                    dp[i][j] = costs[i][j] + Math.min(dp[i-1][j-1], dp[i-1][j+1]); 
-                }
-                else{
-                    dp[i][j] = costs[i][j] + Math.min(dp[i-1][j-1], dp[i-1][j-2]); 
-                }
+            for(int j = 0; j < m; j++){
+                if(j == 0)
+                    dp[i][j] = costs[i][j] + Math.min(dp[i-1][j+1], dp[i-1][j+2]);
+                else if(j == 1)
+                    dp[i][j] = costs[i][j] + Math.min(dp[i-1][j+1], dp[i-1][j-1]);
+                else
+                    dp[i][j] = costs[i][j] + Math.min(dp[i-1][j-1], dp[i-1][j-2]);
             }
-            
         }
+        int totalMinCost = Integer.MAX_VALUE;
         
-        for(int k = 0; k < 3; k++){
-            ans = Math.min(dp[n-1][k], ans);
+        for(int i = 0; i < 3; i++){
+            totalMinCost = Math.min(totalMinCost, dp[n-1][i]);
         }
-        
-        return ans;
+        return totalMinCost;
     }
 }
