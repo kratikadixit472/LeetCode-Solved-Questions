3b5f83a6a6f970ebb8a3f9680d3068acba3d7579
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-04 14:39:21 +0530

    Time: 77 ms (47.91%), Space: 124.8 MB (58.68%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.java
@@ -5,7 +5,7 @@ class Solution {
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0]+a[1] - b[0]-b[1]);
         
-        for(int i = 0; i < num1.length; i++) pq.add(new int[]{num1[i], num2[0], 0});
+        for(int i = 0; i < num1.length && i < k; i++) pq.add(new int[]{num1[i], num2[0], 0});
         
         while(k-- > 0 && !pq.isEmpty()){
             
