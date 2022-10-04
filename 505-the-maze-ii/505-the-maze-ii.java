b4950075c936fef84414bc3897c364277d2957e9
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-04 15:20:32 +0530

    Time: 16 ms (51.34%), Space: 54.7 MB (30.23%) - LeetHub

--- 505-the-maze-ii/505-the-maze-ii.java
+++ 505-the-maze-ii/505-the-maze-ii.java
@@ -4,43 +4,43 @@ class Solution {
         int n = maze.length, m = maze[0].length;
         
         Queue<int[]> q = new LinkedList<>();
-        int[][] dir = {{-1,0}, {0,-1}, {0,1}, {1,0}};
+        q.add(new int[]{start[0], start[1]});
         int[][] dis = new int[n][m];
         
-        for(int i = 0; i < n; i++){
-            Arrays.fill(dis[i], Integer.MAX_VALUE);
-        }
-        
-        q.add(start);
-        int distance = 0;
+        for(int[] d : dis) Arrays.fill(d, Integer.MAX_VALUE);
         dis[start[0]][start[1]] = 0;
+        
+        
+        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         
         while(!q.isEmpty()){
             
             int[] top = q.poll();
-            int i = top[0], j = top[1];
-
-            // if(i == dest[0] && j == dest[1]) return dis[dest[0]][dest[1]];
-
-            for(int[] d : dir) {
-                int r = i + d[0];
-                int c = j + d[1];
-
-                int cnt = 0;
-
-                while(r >= 0 && c >= 0 && r < n && c < m && maze[r][c] == 0) {
-
-                    r += d[0]; 
-                    c += d[1];
-                    cnt++;
-
-                }
-                if(dis[i][j] + cnt < dis[r-d[0]][c-d[1]]) {
-                    q.add(new int[]{r-d[0], c-d[1]});
-                    dis[r-d[0]][c-d[1]] = dis[i][j] + cnt;
-                }
-            }
             
+            int i = top[0], j = top[1];
+            
+            // if(i == dest[0] && j == dest[1]) return distance;  
+            
+            for(int[] d : dir){
+                
+                int row = i + d[0];
+                int col = j + d[1];
+                int cnt = 0;
+                
+                while(row >= 0 && col >= 0 && row < n && col < m && maze[row][col] == 0){
+                    cnt++;
+                    row += d[0];
+                    col += d[1];
+                }
+                
+                row -= d[0]; col -= d[1];
+                
+                if(dis[row][col] > cnt + dis[i][j]){
+                    q.add(new int[]{row, col});
+                    dis[row][col] = cnt + dis[i][j];
+                }
+                
+            }
         }
         return dis[dest[0]][dest[1]] == Integer.MAX_VALUE ? -1 : dis[dest[0]][dest[1]];
     }
