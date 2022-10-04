8066deaa157b65fb4ef39831695792f06a98c2d2
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-04 15:24:56 +0530

    Time: 12 ms (29.30%), Space: 54.1 MB (55.50%) - LeetHub

--- 490-the-maze/490-the-maze.java
+++ 490-the-maze/490-the-maze.java
@@ -1,33 +1,42 @@
 class Solution {
     public boolean hasPath(int[][] maze, int[] start, int[] dest) {
-        
         int n = maze.length, m = maze[0].length;
         
         Queue<int[]> q = new LinkedList<>();
-        int[][] dir = {{-1,0}, {0,-1}, {0,1}, {1,0}};
+        q.add(new int[]{start[0], start[1]});
         boolean[][] vis = new boolean[n][m];
         
-        q.add(start);
+        // for(int[] d : dis) Arrays.fill(d, Integer.MAX_VALUE);
+        vis[start[0]][start[1]] = true;
+        
+        
+        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         
         while(!q.isEmpty()){
             
             int[] top = q.poll();
             
-            if(top[0] == dest[0] && top[1] == dest[1]) return true;
+            int i = top[0], j = top[1];
+            
+            if(i == dest[0] && j == dest[1]) return true;  
             
             for(int[] d : dir){
-                int r = top[0] + d[0];
-                int c = top[1] + d[1];
                 
-                while(r >= 0 && c >= 0 && r < n && c < m && maze[r][c] == 0){
-                    
-                    r += d[0]; c += d[1];
-            
+                int row = i + d[0];
+                int col = j + d[1];
+                
+                while(row >= 0 && col >= 0 && row < n && col < m && maze[row][col] == 0){
+                    row += d[0];
+                    col += d[1];
                 }
-                if(!vis[r-d[0]][c-d[1]]){
-                    q.add(new int[]{r-d[0], c-d[1]});
-                    vis[r-d[0]][c-d[1]] = true;
-                }
+                
+                row -= d[0]; col -= d[1];
+                
+                if(vis[row][col]) continue;
+                
+                q.add(new int[]{row, col});
+                vis[row][col] = true;
+                 
             }
         }
         return false;
