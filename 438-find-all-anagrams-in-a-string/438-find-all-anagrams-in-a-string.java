aee893d5cadc45dbffb4afb9016d0ce9a0aae53c
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-04 22:30:58 +0530

    Time: 99 ms (24.42%), Space: 48.4 MB (36.37%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,35 +1,32 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         
-        List<Integer> ans = new ArrayList<>();
-        int left = 0, matched = 0;
         Map<Character, Integer> map = new HashMap<>();
         
         for(char ch : p.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
         
+        int matched = 0, left = 0;
+        List<Integer> ans = new ArrayList<>();
+        
         for(int right = 0; right < s.length(); right++){
             char ch = s.charAt(right);
             if(map.containsKey(ch)){
-                map.put(ch, map.getOrDefault(ch, 0) - 1);
+                map.put(ch, map.getOrDefault(ch, 0)-1);
                 if(map.get(ch) == 0) matched++;
             }
-            if(matched == map.size()) {
+            if(matched == map.size()){
                 ans.add(left);
-                //left = right;
-                //matched = 0;
             }
             if(right >= p.length()-1){
-                char c = s.charAt(left);
-                left++;
+                char c = s.charAt(left++);
                 if(map.containsKey(c)){
                     if(map.get(c) == 0) matched--;
-                    map.put(c, map.getOrDefault(c, 0) + 1);
+                    map.put(c, map.getOrDefault(c, 0)+1);
                 }
             }
         }
-        
         return ans;
     }
 }
