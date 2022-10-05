a4655afa8d940008ab04996675f4be2338c46204
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-05 15:28:20 +0530

    Time: 16 ms (50.39%), Space: 42.4 MB (95.77%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -2,46 +2,53 @@ class Solution {
     public int longestIncreasingPath(int[][] matrix) {
         
         int n = matrix.length, m = matrix[0].length;
-        
         int[][] indegree = new int[n][m];
-        int[][] vis = {{-1,0},{0,-1},{1,0},{0,1}};
+        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                for(int[] d : vis){
-                    int r = d[0] + i;
-                    int c = d[1] + j;
+                
+                for(int[] d : dir){
+                    int r = i + d[0];
+                    int c = j + d[1];
                     
-                    if(r >= 0 && c >= 0 && r < n && c < m && matrix[i][j] > matrix[r][c]){
-                        indegree[i][j]++;
+                    if(r >= 0 && c >= 0 && r < n && c < m && matrix[i][j] < matrix[r][c]){
+                        indegree[r][c]++;
                     }
                 }
             }
         }
         
         LinkedList<int[]> q = new LinkedList<>();
+        
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(indegree[i][j] == 0) q.add(new int[]{i, j});
+                if(indegree[i][j] == 0){
+                    q.add(new int[]{i, j});
+                }
             }
         }
-        int longest = 0;
+        
+        int longestInc = 0;
         
         while(!q.isEmpty()){
             int sz = q.size();
-                while(sz-- > 0){
+            while(sz-- > 0){
+                
                 int[] top = q.poll();
-                for(int[] d : vis){
-                    int r = d[0] + top[0];
-                    int c = d[1] + top[1];
-
-                    if(r >= 0 && c >= 0 && r < n && c < m && matrix[top[0]][top[1]] < matrix[r][c] && --indegree[r][c] == 0){
-                        q.add(new int[]{r, c});
+                int r = top[0], c = top[1];
+                
+                for(int[] d : dir){
+                    int row = r + d[0];
+                    int col = c + d[1];
+                    
+                    if(row >= 0 && col >= 0 && row < n && col < m && matrix[row][col] > matrix[r][c]){
+                        if(--indegree[row][col] == 0) q.add(new int[]{row, col});
                     }
                 }
             }
-            longest++;
+            longestInc++;
         }
-        return longest;
+        return longestInc;
     }
 }
