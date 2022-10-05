f442f61246f9ad5cac8090475c244b0639472aaa
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-05 19:43:34 +0530

    Time: 52 ms (69.75%), Space: 66 MB (14.31%) - LeetHub

--- 359-logger-rate-limiter/359-logger-rate-limiter.java
+++ 359-logger-rate-limiter/359-logger-rate-limiter.java
@@ -1,26 +1,18 @@
 class Logger {
 
-    HashMap<String, Integer> map;
+    Map<String, Integer> map;
     
     public Logger() {
         map = new HashMap<>();
     }
     
     public boolean shouldPrintMessage(int timestamp, String message) {
-
+        
         if(map.containsKey(message)){
-            if(map.get(message) <= timestamp){
-                map.put(message, timestamp+10);
-                return true;
-            }
-            else{
-                return false;
-            }
+            if(map.get(message) > timestamp) return false;
         }
-        else{
-            map.put(message, timestamp+10);
-            return true;
-        }
+        map.put(message, timestamp+10);
+        return true;
     }
 }
 
