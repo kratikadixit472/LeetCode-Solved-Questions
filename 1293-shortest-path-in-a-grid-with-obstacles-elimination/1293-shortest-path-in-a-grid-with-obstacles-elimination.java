acab61f451ccc6f61003533e12853699469a46bb
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-05 11:30:18 +0530

    Time: 63 ms (45.57%), Space: 55.5 MB (32.33%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,39 +1,38 @@
 class Solution {
-    public int shortestPath(int[][] grid, int K) {
+    public int shortestPath(int[][] grid, int k) {
         
         int n = grid.length, m = grid[0].length;
-        LinkedList<int[]> q = new LinkedList<>();
+        
+        Queue<int[]> pq = new LinkedList<>();
+        boolean[][][] vis = new boolean[n][m][k+1];
+        
         int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-        int[][] vis = new int[n][m];
         
-        for(int[] v : vis) Arrays.fill(v, Integer.MAX_VALUE);
+        pq.add(new int[]{0, 0, 0, 0});
+        vis[0][0][0] = true;
         
-        vis[0][0] = 0;
-        q.add(new int[]{0, 0, 0});
-        int distance = 0;
-        
-        while(!q.isEmpty()){
-            int sz = q.size();
-            while(sz-- > 0){
-                int[] top = q.poll();
-                int i = top[0], j = top[1], k = top[2];
-
-                // System.out.println(i+" "+ j +" "+ k +" "+ dis);
-                if(i == n-1 && j == m-1) return distance;
-                for(int[] d : dir){
-                    int r = i + d[0];
-                    int c = j + d[1];
-
-                    if(r >= 0 && c >= 0 && r < n && c < m){
-                        int newk = k + grid[r][c];
-                        if(newk <= K && vis[r][c] > newk){
-                            vis[r][c] = newk;
-                            q.add(new int[]{r, c, newk});
-                        }
+        while(!pq.isEmpty()){
+            int[] top = pq.poll();
+            
+            int r = top[0], c = top[1], steps = top[2], currK = top[3];
+            
+            if(r == n-1 && c == m-1) return steps;
+            
+            for(int[] d : dir){
+                int nr = r + d[0];
+                int nc = c + d[1];
+                
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m){
+                    
+                    int nextK = currK + grid[nr][nc];
+                    
+                    if(nextK <= k && !vis[nr][nc][nextK]){
+                        pq.add(new int[]{nr, nc, steps+1, nextK});
+                        vis[nr][nc][nextK] = true;
                     }
+                    
                 }
             }
-            distance++;
         }
         return -1;
     }
