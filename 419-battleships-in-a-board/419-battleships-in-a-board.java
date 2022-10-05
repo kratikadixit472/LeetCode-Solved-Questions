19be106c73cca9e9b2cacf2c55a0f7826b39ef14
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-05 18:09:05 +0530

    Time: 1 ms (93.35%), Space: 44.1 MB (67.00%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.java
+++ 419-battleships-in-a-board/419-battleships-in-a-board.java
@@ -1,22 +1,19 @@
 class Solution {
-   
     public int countBattleships(char[][] board) {
         
         int n = board.length, m = board[0].length;
-        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         int ans = 0;
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(board[i][j] == 'X'){
+                    
                     if(i > 0 && board[i-1][j] == 'X') continue;
                     if(j > 0 && board[i][j-1] == 'X') continue;
                     ans++;
-                    
                 }
             }
         }
         return ans;
     }
-    
 }
