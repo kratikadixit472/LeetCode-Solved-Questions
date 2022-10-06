24896ba01984e6068ccb95d99e612e6229241a7d
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-06 05:52:05 +0530

    Time: 35 ms (15.52%), Space: 127.7 MB (34.86%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
@@ -2,37 +2,38 @@ class Solution {
     public long maxPoints(int[][] points) {
         
         int n = points.length, m = points[0].length;
+        long[][] dp = new long[n][m];
+        long maxPoints = 0;
         
-        long[] dp = new long[m];
-        
-        for(int i = 0; i < points[0].length; i++){
-            dp[i] = points[0][i];
+        for(int j = 0; j < m; j++){
+            dp[0][j] = points[0][j];
+            // maxPoints = Math.max(maxPoints, points[0][j]);
         }
-        long maxAns = dp[0];
         
-        for(int i = 0; i < n-1; i++){
-            long[] curr = new long[m];
-            long[] right = new long[m];
+        for(int i = 1; i < n; i++){
+            
             long[] left = new long[m];
+            long[] right = new long[m];
+
+            left[0] = dp[i-1][0];
+            right[m-1] = dp[i-1][m-1];
             
-            left[0] = dp[0];
-            right[m-1] = dp[m-1];
-            
-            for(int j = 1; j < m; j++){
-                left[j] = Math.max(dp[j], left[j-1]-1); 
+            for(int k = 1; k < m; k++){
+                left[k] = Math.max(dp[i-1][k], left[k-1]-1);
             }
-            for(int j = m-2; j >= 0; j--){
-                right[j] = Math.max(dp[j], right[j+1]-1); 
+            for(int k = m-2 ; k >= 0 ; k--){
+                right[k] = Math.max(dp[i-1][k], right[k+1]-1);
             }
+
             for(int j = 0; j < m; j++){
-                curr[j] = points[i+1][j] + Math.max(left[j], right[j]); 
+                dp[i][j] = points[i][j] + Math.max(left[j], right[j]);
             }
-            dp = curr;
         }
         
         for(int j = 0; j < m; j++){
-            maxAns = Math.max(maxAns, dp[j]);
+            maxPoints = Math.max(maxPoints, dp[n-1][j]);
         }
-        return maxAns;
+    
+        return maxPoints;
     }
 }
