520e28ac450d3ed059e2336533fe53bed856cbdc
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-06 22:26:20 +0530

    Time: 34 ms (53.47%), Space: 48.1 MB (38.42%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.java
@@ -2,26 +2,27 @@ class Solution {
     public int numSplits(String s) {
         
         int n = s.length();
-        int count = 0;
         
         int[] pref = new int[n];
         int[] suff = new int[n];
+        
         Set<Character> set = new HashSet<>();
         
-        for(int i =0 ; i < n; i++){
+        for(int i = 0; i < n; i++){
             set.add(s.charAt(i));
             pref[i] = set.size();
         }
         set.clear();
         
-        for(int i = n-1; i >= 0; i--){
+        for(int i = n-1; i >= 0 ; i--){
             set.add(s.charAt(i));
             suff[i] = set.size();
         }
+        int ans = 0;
         
-        for(int i = 1 ; i < n; i++){
-            if(pref[i-1] == suff[i]) count++;
+        for(int i = 1; i < n; i++){
+            if(pref[i-1] == suff[i]) ans++;
         }
-        return count;
+        return ans;
     }
 }
