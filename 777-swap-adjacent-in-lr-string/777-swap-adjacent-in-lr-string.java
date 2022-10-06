f68c00672599a07a3e2604125c75df97fb669f6a
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-06 22:41:50 +0530

    Time: 2 ms (100.00%), Space: 42.4 MB (92.42%) - LeetHub

--- 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.java
+++ 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean canTransform(String start, String end) {
-        if(!start.replace("X", "").equals(end.replace("X", ""))) return false;
         
-        int p1 = 0, p2 = 0;
+        char[] s = start.toCharArray();
+        char[] t = end.toCharArray();
         
-        while(p1 < start.length() && p2 < end.length()){
+        int n = s.length, m = t.length;
+        int i = 0, j = 0;
+        
+        while(i < n || j < m){
             
-            while(p1 < start.length() && start.charAt(p1) == 'X') p1++;
+            while(i < n && s[i] == 'X') i++;
+            while(j < m && t[j] == 'X') j++;
             
-            while(p2 < end.length() && end.charAt(p2) == 'X') p2++;
+            if(i == n && j == m) return true;
+            if(i == n || j == m) return false;
             
-            if(p1 == start.length() && p2 == end.length()) return true;
+            if(s[i] != t[j]) return false;
             
-            if(p1 == start.length() || p2 == end.length()) return false;
+            if(s[i] == 'L' && i < j) return false;
+            if(s[i] == 'R' && i > j) return false;
             
-            if(start.charAt(p1) != end.charAt(p2)) return false;
-            
-            if(start.charAt(p1) == 'L' && p2 > p1) return false;
-            
-            if(start.charAt(p1) == 'R' && p1 > p2) return false;
-            
-            p1++; p2++;
+            i++; j++;
         }
         return true;
     }
