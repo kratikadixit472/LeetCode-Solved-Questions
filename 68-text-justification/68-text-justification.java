b3378b550bb2bfffc3155087591563f4c3755ab4
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-06 22:00:21 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (87.68%) - LeetHub

--- 68-text-justification/68-text-justification.java
+++ 68-text-justification/68-text-justification.java
@@ -2,43 +2,43 @@ class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         
         List<String> ans = new ArrayList<>();
+        int n = words.length, index = 0; 
         
-        int index = 0, n = words.length;
-        
-        while(index < n){
+        while(index < words.length){
             
-            int last = index + 1;
             int count = words[index].length();
+            int last = index + 1;
             
             while(last < n){
                 if(count + 1 + words[last].length() <= maxWidth){
-                    count += 1 + words[last].length();
+                    count +=  1 + words[last].length();
                     last++;
                 }
                 else break;
             }
-
+            
             StringBuilder sb = new StringBuilder();
             sb.append(words[index]);
-            
             int diff = last - index - 1;
+            
             if(last == n || diff == 0){
                 
-                for(int i = index+1; i < last; i++){
+                for(int i = index + 1; i < last; i++){
                     sb.append(" ");
                     sb.append(words[i]);
                 }
-  
-                for(int i = sb.length(); i<maxWidth; i++){
+                for(int i = sb.length(); i < maxWidth; i++){
                     sb.append(" ");
                 }
             }
+            
             else{
+                
                 int spaces = (maxWidth - count) / diff;
                 int odd = (maxWidth - count) % diff;
                 
-                for(int i = index+1; i < last; i++){
-                    for(int k = spaces; k > 0; k--){
+                for(int i = index + 1; i < last; i++){
+                    for(int k = spaces; k > 0 ; k--){
                         sb.append(" ");
                     }
                     if(odd > 0){
