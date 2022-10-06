b15b0297e55b133e2b3702503b9f18079cc9eaf5
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-06 18:53:54 +0530

    Time: 3 ms (79.04%), Space: 43.2 MB (43.12%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.java
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.java
@@ -2,15 +2,20 @@ class Solution {
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
         
         Map<Integer, Integer> map = new HashMap<>();
+        
         for(int i = 0; i < indices.length; i++){
-            if(s.startsWith(sources[i], indices[i])){
+            int pattern = indices[i];
+            int len = sources[i].length();
+            if(indices[i] + len > s.length()) continue;
+            String subs = s.substring(indices[i], indices[i]+len);
+            if(subs.equals(sources[i])){
                 map.put(indices[i], i);
             }
         }
         
         StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i < s.length();){
+        for(int i = 0; i < s.length(); ){
             if(map.containsKey(i)){
                 sb.append(targets[map.get(i)]);
                 i += sources[map.get(i)].length();
@@ -23,3 +28,12 @@ class Solution {
         return sb.toString();
     }
 }
+
+/*
+Input:
+"abcd"
+[0,1]
+["ab","bc"]
+["eee","ffff"]
+Expected: "affffd"
+*/
