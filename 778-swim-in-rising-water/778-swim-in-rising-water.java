951a6a79b6adc3c206961925d973bbd485a0eb85
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-07 17:29:00 +0530

    Time: 20 ms (60.36%), Space: 45.3 MB (58.38%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.java
+++ 778-swim-in-rising-water/778-swim-in-rising-water.java
@@ -2,31 +2,33 @@ class Solution {
     public int swimInWater(int[][] grid) {
         
         int n = grid.length, m = grid[0].length;
+        int minTime = Integer.MIN_VALUE;
+        int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        pq.add(new int[]{grid[0][0], 0, 0});
-        int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         boolean[][] vis = new boolean[n][m];
         
-        int ans = 0;
+        pq.add(new int[]{0, 0, grid[0][0]});
+        vis[0][0] = true;
+        
         while(!pq.isEmpty()){
             
             int[] top = pq.poll();
-            int t = top[0], i = top[1], j = top[2];
-            ans = Math.max(ans, grid[i][j]);
+            int i = top[0], j = top[1], time = top[2];
             
-            if(i == n-1 && j == m-1) return ans;
+            minTime = Math.max(minTime, time);
+            if(i == n-1 && j == m-1) return minTime;
             
             for(int[] d : dir){
                 int r = i + d[0];
                 int c = j + d[1];
                 
-                if(r >= 0 && c >= 0 && r < n &&  c < m && !vis[r][c]){
-                    pq.add(new int[]{grid[r][c], r, c});
+                if(r >= 0 && c >= 0 && r < n && c < m && !vis[r][c]){
                     vis[r][c] = true;
+                    pq.add(new int[]{r, c, grid[r][c]});
                 }
             }
         }
-        return 0;
+        return -1;
     }
 }
