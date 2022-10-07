dab88ddd9e5ff6b161ecec4b17b1509ca1505bea
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-08 01:26:17 +0530

    Time: 116 ms (74.75%), Space: 42.9 MB (93.56%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.java
@@ -0,0 +1,32 @@
+class MyCalendarThree {
+
+    Map<Integer, Integer> map;
+    
+    public MyCalendarThree() {
+        map = new TreeMap<>();
+    }
+    
+    public int book(int start, int end) {
+        map.put(start, map.getOrDefault(start, 0) + 1);
+        map.put(end, map.getOrDefault(end, 0) - 1);
+        
+        int curr = 0, res = 0;
+        
+        for(int diff : map.values()){
+            curr += diff;
+            res = Math.max(res, curr);
+        }
+        return res;
+    }
+}
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(start,end);
+ 
+ ["MyCalendarThree", "book", "book", "book", "book", "book", "book"]
+[[], [10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]]
+Output
+[null, 1, 1, 2, 3, 3, 3]
+ */
