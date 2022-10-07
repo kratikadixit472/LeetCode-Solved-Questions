68499a7f503a7bd0448948b4b075d618dae0fbbc
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-07 18:18:25 +0530

    Time: 25 ms (48.75%), Space: 55 MB (74.48%) - LeetHub

--- 562-longest-line-of-consecutive-one-in-matrix/562-longest-line-of-consecutive-one-in-matrix.java
+++ 562-longest-line-of-consecutive-one-in-matrix/562-longest-line-of-consecutive-one-in-matrix.java
@@ -1,15 +1,16 @@
 class Solution {
-    
-    public int longestLine(int[][] mat) {      
-        int n = mat.length, m = mat[0].length;
-        int max = 0;
+    public int longestLine(int[][] mat) {
+        
+        int max = 0, n = mat.length, m = mat[0].length;
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(mat[i][j] == 1){
-                    int horz = 0, vert = 0, diag = 0, anti = 0;
+                if(mat[i][j] == 0) continue;
+
+                else{
+                    int cnt = 0;
                     if(i == 0 || mat[i-1][j] == 0){
-                        int row = i;
+                        int row = i, horz = 0;
                         while(row < n && mat[row][j] == 1){
                             row++;
                             horz++;
@@ -17,7 +18,7 @@ class Solution {
                         max = Math.max(max, horz);
                     }
                     if(j == 0 || mat[i][j-1] == 0){
-                        int col = j;
+                        int col = j, vert = 0;
                         while(col < m && mat[i][col] == 1){
                             col++;
                             vert++;
@@ -25,21 +26,24 @@ class Solution {
                         max = Math.max(max, vert);
                     }
                     if(i == 0 || j == 0 || mat[i-1][j-1] == 0){
-                        int row = i, col = j;
+                        int row = i, col = j, diag = 0;
                         while(row < n && col < m && mat[row][col] == 1){
-                            row++;col++;
+                            row++;
+                            col++;
                             diag++;
                         }
                         max = Math.max(max, diag);
                     }
                     if(i == 0 || j == m-1 || mat[i-1][j+1] == 0){
-                        int row = i, col = j;
+                        int row = i, col = j, adiag = 0;
                         while(row < n && col >= 0 && mat[row][col] == 1){
-                            row++; col--;
-                            anti++;
+                            row++;
+                            col--;
+                            adiag++;
                         }
-                        max = Math.max(max, anti);
+                        max = Math.max(max, adiag);
                     }
+                    
                 }
             }
         }
