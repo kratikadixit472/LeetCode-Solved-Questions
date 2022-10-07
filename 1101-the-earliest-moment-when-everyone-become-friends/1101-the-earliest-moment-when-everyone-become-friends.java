231ab90e4f5dcc7618dcf87e0141edad041a4f96
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-07 20:03:57 +0530

    Time: 11 ms (50.81%), Space: 46.9 MB (75.10%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.java
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.java
@@ -1,27 +1,26 @@
 class Solution {
     
     int[] par;
+    
     private int findPar(int u){
         return (par[u] == u) ? u : (par[u] = findPar(par[u]));
     }
     
     public int earliestAcq(int[][] logs, int n) {
+        
         par = new int[n];
         
+        for(int i = 0; i < n; i++) par[i] = i;
+        
         Arrays.sort(logs, (a, b) -> a[0] - b[0]);
-        
-        for(int i = 0; i < n; i++){
-            par[i] = i;
-        }
-        
-        
+        int earlierTime = Integer.MIN_VALUE;
         
         for(int[] log : logs){
-            int time = log[0], x = log[1], y = log[2];
-            int p1 = findPar(log[1]), p2 = findPar(log[2]);
+            int x = findPar(log[1]), y = findPar(log[2]);
             
-            if(p1 != p2){
-                par[p1] = par[p2];
+            int time = log[0];
+            if(x != y){
+                par[x] = par[y];
                 n--;
             }
             if(n == 1) return time;
@@ -29,3 +28,8 @@ class Solution {
         return -1;
     }
 }
+
+/*
+[[7,3,1],[2,3,0],[3,2,1],[6,0,1],[0,2,0],[4,3,2]]
+4
+*/
