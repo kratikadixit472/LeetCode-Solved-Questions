af1deb8d2965ec2861b227aa5d65e552a642272e
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-07 23:23:56 +0530

    Time: 8 ms (50.06%), Space: 43.2 MB (17.69%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.java
+++ 269-alien-dictionary/269-alien-dictionary.java
@@ -2,47 +2,48 @@ class Solution {
     public String alienOrder(String[] words) {
         
         int n = words.length;
+        Map<Character, List<Character>> map = new HashMap<>();
         Map<Character, Integer> indegree = new HashMap<>();
-        Map<Character, List<Character>> graph = new HashMap<>();
-        
         for(String word : words){
             for(char c : word.toCharArray()){
-                graph.putIfAbsent(c, new ArrayList<>());
+                map.put(c, new ArrayList<>());
                 indegree.put(c, 0);
             }
+            
         }
-        
         for(int i = 0; i < n-1; i++){
-            String w1 = words[i], w2 = words[i+1];
+            String s1 = words[i], s2 = words[i+1];
             
-            if(w1.length() > w2.length() && w1.startsWith(w2)) return "";
+            if(s1.length() > s2.length() && s1.startsWith(s2)) return "";
             
-            for(int k = 0; k < Math.min(w1.length(), w2.length()) ;k++){
-                char c1 = w1.charAt(k), c2 = w2.charAt(k);
-                if(c1 != c2){
-                    graph.get(c1).add(c2);
-                    indegree.put(c2, indegree.getOrDefault(c2, 0) + 1);
+            for(int j = 0; j < Math.min(s1.length(), s2.length()); j++){
+                if(s1.charAt(j) != s2.charAt(j)){
+                    map.get(s1.charAt(j)).add(s2.charAt(j));
+                    indegree.put(s2.charAt(j), indegree.get(s2.charAt(j))+1);
                     break;
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
+        
         LinkedList<Character> q = new LinkedList<>();
         
         for(char key : indegree.keySet()){
             if(indegree.get(key) == 0) q.add(key);
         }
+        StringBuilder ans = new StringBuilder();
         
         while(!q.isEmpty()){
             char top = q.poll();
-            sb.append(top);
             
-            for(char c : graph.get(top)){
-                indegree.put(c, indegree.getOrDefault(c, 0) - 1);
-                if(indegree.get(c) == 0) q.add(c);
+            ans.append(top);
+            for(char next : map.get(top)){
+                indegree.put(next, indegree.getOrDefault(next,0)-1);
+                if(indegree.get(next) == 0) q.add(next);
             }
         }
-        if(sb.length() < indegree.size()) return "";
-        return sb.toString();
+        
+        if(ans.length() < indegree.size()) return "";
+        
+        return ans.toString();
     }
 }
