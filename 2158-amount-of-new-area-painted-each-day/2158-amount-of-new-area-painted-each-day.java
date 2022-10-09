e4de8f02d7462d6bf8e62c8d18999fe54afa71d7
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-09 20:59:15 +0530

    Time: 30 ms (77.05%), Space: 146.4 MB (71.74%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.java
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.java
@@ -1,18 +1,23 @@
 class Solution {
     public int[] amountPainted(int[][] paint) {
         
-        int[] line = new int[50001];
-        int[] ans = new int[paint.length];
+        int[] line = new int[100005];
+        int idx = 0,  n = paint.length;
         
-        for(int i = 0; i < paint.length; i++){
-            int start = paint[i][0], end = paint[i][1];
+        int[] ans = new int[n];
+        
+        for(int[] p : paint){
+            int start = p[0], end = p[1];
+            
             while(start < end){
-                int jump = Math.max(start+1, line[start]);
-                ans[i] += (line[start] == 0) ? 1 : 0;
                 
+                int jump = Math.max(start+1 ,line[start]);
+                if(line[start] == 0) ans[idx]++;
                 line[start] = Math.max(end, line[start]);
+                
                 start = jump;
             }
+            idx++;
         }
         return ans;
     }
