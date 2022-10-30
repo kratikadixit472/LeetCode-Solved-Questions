90c49881366d63e1fc9aae395ad29491967b3f86
Kratika Dixit <kratikadixit472@gmail.com>
2022-10-31 02:09:15 +0530

    Time: 20 ms (82.63%), Space: 47.6 MB (59.67%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,36 +1,33 @@
 class Solution {
     public int shortestPath(int[][] grid, int k) {
-        
-        int n = grid.length, m = grid[0].length;
-        
-        Queue<int[]> pq = new LinkedList<>();
-        boolean[][][] vis = new boolean[n][m][k+1];
-        
         int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         
-        pq.add(new int[]{0, 0, 0, 0});
-        vis[0][0][0] = true;
+        int n = grid.length, m = grid[0].length;
+        int[][] steps = new int[n][m];
+        for(int[] step : steps) Arrays.fill(step, Integer.MAX_VALUE);
         
-        while(!pq.isEmpty()){
-            int[] top = pq.poll();
+        LinkedList<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, 0, 0});
+        steps[0][0] = 0;
+        
+        while(!q.isEmpty()){
             
-            int r = top[0], c = top[1], steps = top[2], currK = top[3];
+            int[] top = q.poll();
+            int i = top[0], j = top[1], dis = top[3], currk = top[2];
             
-            if(r == n-1 && c == m-1) return steps;
+            if(i == n-1 && j == m-1) return dis;
+            if(currk > k) continue;
             
             for(int[] d : dir){
-                int nr = r + d[0];
-                int nc = c + d[1];
+                int r = i + d[0];
+                int c = j + d[1];
                 
-                if(nr >= 0 && nc >= 0 && nr < n && nc < m){
-                    
-                    int nextK = currK + grid[nr][nc];
-                    
-                    if(nextK <= k && !vis[nr][nc][nextK]){
-                        pq.add(new int[]{nr, nc, steps+1, nextK});
-                        vis[nr][nc][nextK] = true;
+                if(r >= 0 && c >= 0 && r < n && c < m){
+                    int newk = grid[r][c] + currk;
+                    if(newk <= k && newk < steps[r][c]){
+                        steps[r][c] = newk;
+                        q.add(new int[]{r, c, newk, dis + 1});
                     }
-                    
                 }
             }
         }
