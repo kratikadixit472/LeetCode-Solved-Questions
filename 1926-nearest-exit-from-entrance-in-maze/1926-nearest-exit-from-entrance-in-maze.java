fb535bad9cb135d66ee1066dd0ff49d0214e2b1d
Kratika Dixit <kratikadixit472@gmail.com>
2022-11-21 19:02:01 +0530

    Time: 29 ms (21.40%), Space: 71.8 MB (46.66%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,37 +1,32 @@
 class Solution {
     public int nearestExit(char[][] maze, int[] entrance) {
         
+        int exit = 0, n = maze.length, m = maze[0].length;
         LinkedList<int[]> q = new LinkedList<>();
+        int[][] dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        boolean[][] vis = new boolean[n][m];
+          
         q.add(new int[]{entrance[0], entrance[1]});
-        maze[entrance[0]][entrance[1]] = '+';
-        
-        int n = maze.length, m = maze[0].length;
-        int[][] dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        int near = 0;
+        vis[entrance[0]][entrance[1]] = true;
         
         while(!q.isEmpty()){
             int sz = q.size();
             while(sz-- > 0){
+                
                 int[] curr = q.poll();
                 
-                int i = curr[0], j = curr[1];
-                
                 for(int[] d : dir){
-                    int r = i + d[0];
-                    int c = j + d[1];
+                    int r = curr[0] + d[0];
+                    int c = curr[1] + d[1];
                     
-                    if(r < 0 || c < 0 || r >= n || c >= m || maze[r][c] == '+' ) continue;
-                    
-                    if((r == 0 || c == 0 || r == n-1 || c == m-1)) return near + 1;
-                    
-                    if(r >= 0 && c >= 0 && r < n && c < m && maze[r][c] == '.'){          
-                        maze[r][c] = '+';
+                    if(r >= 0 && c >= 0 && r < n && c < m && maze[r][c] == '.' && !vis[r][c]){
+                        if(r == 0 || r == n-1 || c == 0 || c == m-1) return exit + 1;
                         q.add(new int[]{r, c});
+                        vis[r][c] = true;
                     }
-                    
                 }
             }
-            near++;
+            exit++;
         }
         return -1;
     }
