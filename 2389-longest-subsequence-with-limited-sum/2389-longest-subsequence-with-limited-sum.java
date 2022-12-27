8b65dfb58c7ea773cab108efe1b2fc7e6050837d
Kratika Dixit <kratikadixit472@gmail.com>
2022-12-27 17:46:40 +0530

    Time: 5 ms (89.06%), Space: 42.9 MB (63.09%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -5,26 +5,35 @@ class Solution {
         int n = arr.length;
         
         int[] ans = new int[queries.length];
+        int[] prefSum = new int[n];
+        
+        prefSum[0] = arr[0];
+        for(int i = 1; i < n; i++){
+            prefSum[i] = prefSum[i-1] + arr[i];
+        }
+        
         int idx = 0;
         
         for(int sum : queries){
-            
-           int cnt = 0, curr = 0;
-            
-            for(int num : arr){
-                curr += num;
-                cnt++;
-                
-                if(curr == sum) break;
-                else if(curr > sum){
-                    curr = curr - num;
-                    cnt--;
-                }
-            }
-            
-            ans[idx++] = cnt;
+            ans[idx++] = binarySearch(prefSum, sum);
         }
         
         return ans;
+    }
+    
+    private int binarySearch(int[] nums, int sum){
+        
+        int l = 0, r = nums.length-1;
+        while(l < r){
+            int mid = (r + l) / 2;
+            if(nums[mid] == sum) return mid + 1;
+            else if(nums[mid] < sum){
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
+        return nums[l] > sum ? l : l + 1;
     }
 }
