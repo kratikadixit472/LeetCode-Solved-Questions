08df573c21e22e1a8e3aed6a0a2ebe77d1fb82a1
Kratika Dixit <kratikadixit472@gmail.com>
2023-01-01 07:58:34 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (71.61%) - LeetHub

---
+++ 1056-confusing-number/1056-confusing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean confusingNumber(int N) {
+        if(N == 0) return false;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(6, 9); map.put(9, 6); map.put(0, 0); map.put(8, 8); map.put(1, 1);
+        int rev = 0, n = N;
+        while(n != 0){
+            if(n % 10 == 2 || n % 10 == 3 || n % 10 == 4 || n % 10 == 5 || n % 10 == 7) return false;
+            int last = n % 10;
+            rev = rev * 10 + map.get(last);
+            n /= 10;
+        }
+        return (rev != N);
+    }
+}
