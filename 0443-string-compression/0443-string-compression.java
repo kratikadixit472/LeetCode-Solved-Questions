1537d6caffc686404b5fb6bf1917f317c053ce0d
Kratika Dixit <kratikadixit472@gmail.com>
2023-01-01 11:19:30 +0530

    Time: 1 ms (99.16%), Space: 42.1 MB (73.82%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compress(char[] chars) {
+        char prev = ' ';
+        int len = 0, idx = 0, i = 0, n = chars.length;
+        StringBuilder sb = new StringBuilder();
+        
+        while(i < n){
+            int j = i;
+            while(j < n && chars[i] == chars[j]) j++;
+            int cnt = j - i;
+            chars[idx++] = chars[i];
+            
+            if(cnt <= 1) { }
+            
+            else if(cnt < 10){
+                chars[idx++] = (char)(cnt + '0');
+            }
+            else{
+                String s = Integer.toString(cnt);
+                for(char c : s.toCharArray()){
+                    chars[idx++] = c;
+                }
+            }
+            i = j;
+        }
+        
+        return idx;
+    }
+}
