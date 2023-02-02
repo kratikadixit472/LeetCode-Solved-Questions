9deb6af78c9f4f4686ac2bde27c67bcdbc28f051
Kratika Dixit <kratikadixit472@gmail.com>
2023-02-03 00:04:23 +0530

    Time: 1 ms (100.00%), Space: 39.2 MB (97.70%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        while(n > 1){
+            if(n % 2 == 1) return false;
+            n = n / 2;
+        }
+        return (n == 1);
+    }
+}
