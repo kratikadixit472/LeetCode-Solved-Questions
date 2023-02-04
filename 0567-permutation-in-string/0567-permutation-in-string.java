e1abcb4391369299911c64c70bd94ac9ce75bd2a
Kratika Dixit <kratikadixit472@gmail.com>
2023-02-04 10:27:24 +0530

    Time: 4 ms (92.11%), Space: 42.7 MB (41.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         
         int n = s1.length(), m = s2.length();
+        if(m < n) return false;
         
         int[] f1 = new int[26];
         int[] f2 = new int[26];
