7bdb411b3fb73000d8dd037db974ddd58e002c2f
Kratika Dixit <kratikadixit472@gmail.com>
2023-02-09 00:09:34 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (46.50%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.java
+++ 0280-wiggle-sort/0280-wiggle-sort.java
@@ -2,12 +2,14 @@ class Solution {
     public void wiggleSort(int[] nums) {
         
         int n = nums.length;
-        Arrays.sort(nums);
         
-        for(int i = 1;  i < n-1; i += 2){
-            int tmp = nums[i];
-            nums[i] = nums[i+1];
-            nums[i+1] = tmp;
+        for(int i = 0;  i < n-1; i++){
+            if(i % 2 == 0 && nums[i] > nums[i+1] || (i % 2 == 1 && nums[i] < nums[i+1])){
+                int tmp = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = tmp;
+            }
+            
         }
     }
 }
