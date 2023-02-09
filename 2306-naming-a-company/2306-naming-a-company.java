3934d3d624f4abc0e9cc641a3207126a3634287a
Kratika Dixit <kratikadixit472@gmail.com>
2023-02-09 13:09:01 +0530

    Time: 176 ms (93.65%), Space: 53.1 MB (74.60%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.java
@@ -0,0 +1,36 @@
+class Solution {
+    public long distinctNames(String[] ideas) {
+        
+        Set<String>[] set = new HashSet[26];
+        for(int i = 0; i < 26; i++){
+            set[i] = new HashSet<>();
+        }
+        
+        for(String s : ideas){
+            set[s.charAt(0)-'a'].add(s.substring(1));
+        }
+        
+        int n = ideas.length;
+        long ans = 0;
+        
+        for(int i = 0; i < 26; i++){
+            for(int j = i+1; j < 26; j++){
+                
+                long c1 = 0, c2 = 0;
+//                 for(String s : set[i]){
+//                     if(!set[j].contains(s)) c1++;
+//                 }
+                
+//                 for(String s : set[j]){
+//                     if(!set[i].contains(s)) c2++;
+//                 }
+                for(String s : set[i]){
+                    if(set[j].contains(s)) c1++;
+                }
+                
+                ans += (set[i].size() - c1) * (set[j].size() - c1);
+            }
+        }
+        return 2*ans;
+    }
+}
