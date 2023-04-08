06715f144832fa3c97db543f52fdc2d02872aac4
Kratika Dixit <kratikadixit472@gmail.com>
2023-04-08 09:01:53 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (59.93%) - LeetHub

--- 0760-find-anagram-mappings/0760-find-anagram-mappings.java
+++ 0760-find-anagram-mappings/0760-find-anagram-mappings.java
@@ -2,15 +2,14 @@ class Solution {
     public int[] anagramMappings(int[] nums1, int[] nums2) {
         
         int[] ans = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
         
-        for(int i = 0; i < nums1.length; i++){
-            for(int j = 0; j < nums2.length; j++){
-                
-                if(nums1[i] == nums2[j]){
-                    ans[i] = j;
-                    break;
-                }
-            }
+        for(int i = 0; i < nums2.length; i++){
+            map.put(nums2[i], i);
+        }
+        
+        for(int j = 0; j < nums1.length; j++){
+                ans[j] = map.get(nums1[j]);
         }
         return ans;
     }
