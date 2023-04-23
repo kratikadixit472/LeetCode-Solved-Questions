9455b2f61fceac89dfce73b6fa85efaba154d710
Kratika Dixit <kratikadixit472@gmail.com>
2023-04-23 21:26:08 +0530

    Time: 320 ms (37.93%), Space: 44.7 MB (65.52%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numberOfArrays(String s, int k) {
+        int m = s.length(), n = String.valueOf(k).length();
+        int mod = (int)(1e9 + 7);
+        
+        int[] dp = new int[m + 1];
+        
+        dp[0] = 1;
+        
+        for (int start = 0; start < m; ++start) {
+            if (s.charAt(start) == '0')
+                continue;
+            
+            for (int end = start; end < m; ++end) {
+                String currNumber = s.substring(start, end + 1);
+                
+                if (Long.parseLong(currNumber) > k)
+                    break;
+                
+                dp[end + 1] = (dp[end + 1] + dp[start]) % mod;
+            }
+        }
+        return dp[m];
+    }
+}
