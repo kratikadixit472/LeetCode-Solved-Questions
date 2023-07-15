0a78b660f3a5c0cec2d8947289580fd3b6a1aa75
Kratika Dixit <kratikadixit472@gmail.com>
2023-07-16 00:15:56 +0530

    Time: 126 ms (21.49%), Space: 80.1 MB (77.57%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    public int maxValue(int[][] events, int k) {
+        int n = events.length;
+        int[][] dp = new int[k + 1][n + 1];
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
+
+        for (int curIndex = n - 1; curIndex >= 0; --curIndex) {
+            for (int count = 1; count <= k; count++) {
+                int nextIndex = bisectRight(events, events[curIndex][1]);
+                dp[count][curIndex] = Math.max(dp[count][curIndex + 1], events[curIndex][2] + dp[count - 1][nextIndex]);
+            }
+        }
+        return dp[k][0];
+    }
+    
+    private static int bisectRight(int[][] events, int target) {
+        int left = 0, right = events.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (events[mid][0] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    } 
+}
